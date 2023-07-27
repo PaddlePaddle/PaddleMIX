41ede3364e5d228db0b2aeb5fca8829ffba0e663
lyuwenyu <wenyu.lyu@gmail.com>
2023-07-27 15:01:24 +0800

    fix copyright and code style

--- paddlevlp/models/blip2/Qformer.py
+++ paddlevlp/models/blip2/Qformer.py
@@ -1,18 +1,18 @@
-# !/usr/bin/env python3
-# -*- coding:UTF-8 -*-
-################################################################################
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2023 The Salesforce Team Authors and The HuggingFace Team. All rights reserved.
 #
-# Copyright (c) 2023 Baidu.com, Inc. All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-################################################################################
-"""
-Authors:
-        wangsiqi06(wangsiqi06@baidu.com), zhouwenshuo(zhouwenshuo@baidu.com)
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Date:   2023/3/08 17:00:00
-"""
-
-from altair import value
 import sklearn
 import math
 from dataclasses import dataclass
@@ -41,6 +41,8 @@ import numpy as np
 import paddle
 from paddle.distributed.fleet.utils import recompute
 import random
+
+
 class CrossEntropyLoss(nn.Layer):
     """
     Softmax Cross entropy loss
