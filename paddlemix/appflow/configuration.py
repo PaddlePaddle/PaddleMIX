77331d7f4d74ba74deb4d699c8bde0da73e60b6b
LokeZhou <aishenghuoaiqq@163.com>
2023-08-18 10:35:51 +0800

    Merge pull request #84 from LokeZhou/docs
    update applications readme

--- paddlemix/appflow/configuration.py
+++ paddlemix/appflow/configuration.py
@@ -83,7 +83,11 @@ APPLICATIONS = {
             "stabilityai/stable-diffusion-2": {
                 "task_class": StableDiffusionTask,
                 "task_flag": "text2image_generation-stable-diffusion-2",
-            }
+            },
+            "stabilityai/stable-diffusion-v1-5": {
+                "task_class": StableDiffusionTask,
+                "task_flag": "text2image_generation-stable-diffusion-v1-5",
+            },
         },
         "default": {
             "model": "stabilityai/stable-diffusion-2",
