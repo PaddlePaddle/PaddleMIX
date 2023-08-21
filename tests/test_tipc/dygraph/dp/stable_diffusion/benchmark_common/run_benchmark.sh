3e7e6402f60faf933246715be1450cb64b644c67
yujun <573009727@qq.com>
2023-08-21 17:32:31 +0800

    update fused linear

--- tests/test_tipc/dygraph/dp/stable_diffusion/benchmark_common/run_benchmark.sh
+++ tests/test_tipc/dygraph/dp/stable_diffusion/benchmark_common/run_benchmark.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Test training benchmark for a model.
 # Usage：bash benchmark/run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num}
 function _set_params(){
@@ -46,8 +60,11 @@ function _train(){
     if [ ${fp_item} = "fp16" ]; then
         use_fp16_cmd="--fp16 True"
     fi
+
+    FUSED=False
     if [ ${fp_item} = "bf16" ]; then
         use_fp16_cmd="--bf16 True --fp16_opt_level O2"
+        FUSED=True
     fi
     rm -rf ./outputs
 
@@ -55,8 +72,8 @@ function _train(){
     export FLAG_BENCHMARK=1
     export FLAG_RECOMPUTE=1
     export FLAG_XFORMERS=1
-    # use fused linear
-    export FLAG_FUSED_LINEAR=1
+    # use fused linear in amp o2 level
+    export FLAG_FUSED_LINEAR=${FUSED}
 
     train_cmd="../ppdiffusers/examples/stable_diffusion/train_txt2img_laion400m_trainer.py \
             --do_train \
