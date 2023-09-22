ea3073df4691ba5ea02bec784fa9430cba5fd7ca
westfish <westfish@126.com>
2023-09-22 06:51:28 +0000

    tomesd patch

--- ppdiffusers/ppdiffusers/patches/tomesd_patch_utils.py
+++ ppdiffusers/ppdiffusers/patches/tomesd_patch_utils.py
@@ -19,12 +19,12 @@
 # Adapted from https://github.com/dbolya/tomesd
 
 import math
-from typing import Any, Callable, Dict, Tuple, Type, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Type, Union
 
 import paddle
 import paddle.nn as nn
 
-from ..models.transformer_2d import BasicTransformerBlock
+from ..models.attention import BasicTransformerBlock
 from ..pipelines.pipeline_utils import DiffusionPipeline
 from .ppnlp_patch_utils import patch_to
 
@@ -50,7 +50,10 @@ def scatter_reduce(
         input.put_along_axis_(indices=index, values=src, axis=dim, reduce="add")
         # compute div secondly
         input_div = paddle.ones_like(input).put_along_axis(
-            indices=index, values=paddle.to_tensor(1.0, dtype=input.dtype), axis=dim, reduce="add"
+            indices=index,
+            values=paddle.to_tensor(1.0, dtype=input.dtype),
+            axis=dim,
+            reduce="add",
         )
         input = input / input_div
     elif reduce in ["prod", "mul", "multiply"]:
@@ -110,7 +113,9 @@ def bipartite_soft_matching_random2d(
         # The image might not divide sx and sy, so we need to work on a view of the top left if the idx buffer instead
         idx_buffer_view = paddle.zeros([hsy, wsx, sy * sx], dtype=paddle.int64)
         idx_buffer_view.put_along_axis_(
-            axis=2, indices=rand_idx, values=-paddle.ones_like(rand_idx, dtype=rand_idx.dtype)
+            axis=2,
+            indices=rand_idx,
+            values=-paddle.ones_like(rand_idx, dtype=rand_idx.dtype),
         )
         idx_buffer_view = (
             idx_buffer_view.reshape([hsy, wsx, sy, sx]).transpose([0, 2, 1, 3]).reshape([hsy * sy, wsx * sx])
@@ -243,17 +248,19 @@ def make_tome_block(block_class: Type[nn.Layer]) -> Type[nn.Layer]:
 
         def forward(
             self: BasicTransformerBlock,
-            hidden_states,
-            attention_mask=None,
-            encoder_hidden_states=None,
-            encoder_attention_mask=None,
-            timestep=None,
-            cross_attention_kwargs=None,
-            class_labels=None,
-        ) -> paddle.Tensor:
+            hidden_states: paddle.Tensor,
+            attention_mask: Optional[paddle.Tensor] = None,
+            encoder_hidden_states: Optional[paddle.Tensor] = None,
+            encoder_attention_mask: Optional[paddle.Tensor] = None,
+            timestep: Optional[paddle.Tensor] = None,
+            cross_attention_kwargs: Dict[str, Any] = None,
+            class_labels: Optional[paddle.Tensor] = None,
+        ):
             # (1) ToMe
             m_a, m_c, m_m, u_a, u_c, u_m = compute_merge(hidden_states, self._tome_info)
 
+            # Notice that normalization is always applied before the real computation in the following blocks.
+            # 1. Self-Attention
             if self.use_ada_layer_norm:
                 norm_hidden_states = self.norm1(hidden_states, timestep)
             elif self.use_ada_layer_norm_zero:
@@ -266,7 +273,6 @@ def make_tome_block(block_class: Type[nn.Layer]) -> Type[nn.Layer]:
             # (2) ToMe m_a
             norm_hidden_states = m_a(norm_hidden_states)
 
-            # 1. Self-Attention
             cross_attention_kwargs = cross_attention_kwargs if cross_attention_kwargs is not None else {}
             attn_output = self.attn1(
                 norm_hidden_states,
@@ -306,7 +312,20 @@ def make_tome_block(block_class: Type[nn.Layer]) -> Type[nn.Layer]:
             # (6) ToMe m_m
             norm_hidden_states = m_m(norm_hidden_states)
 
-            ff_output = self.ff(norm_hidden_states)
+            if self._chunk_size is not None:
+                # "feed_forward_chunk_size" can be used to save memory
+                if norm_hidden_states.shape[self._chunk_dim] % self._chunk_size != 0:
+                    raise ValueError(
+                        f"`hidden_states` dimension to be chunked: {norm_hidden_states.shape[self._chunk_dim]} has to be divisible by chunk size: {self._chunk_size}. Make sure to set an appropriate `chunk_size` when calling `unet.enable_forward_chunking`."
+                    )
+
+                num_chunks = norm_hidden_states.shape[self._chunk_dim] // self._chunk_size
+                ff_output = paddle.concat(
+                    [self.ff(hid_slice) for hid_slice in norm_hidden_states.chunk(num_chunks, axis=self._chunk_dim)],
+                    axis=self._chunk_dim,
+                )
+            else:
+                ff_output = self.ff(norm_hidden_states)
 
             if self.use_ada_layer_norm_zero:
                 ff_output = gate_mlp.unsqueeze(1) * ff_output
