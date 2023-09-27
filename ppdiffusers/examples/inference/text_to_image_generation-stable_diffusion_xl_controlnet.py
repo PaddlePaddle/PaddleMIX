b42c76797521e1ca821bee1cbd3399fae2e6a77d
CrazyBoyM <ai-lab@foxmail.com>
2023-09-27 10:13:10 +0000

    update test_stable_diffusion_xl and add text_to_image_generation-stable_diffusion_xl_controlnet

--- ppdiffusers/examples/inference/text_to_image_generation-stable_diffusion_xl_controlnet.py
+++ ppdiffusers/examples/inference/text_to_image_generation-stable_diffusion_xl_controlnet.py
@@ -12,4 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# todo
+import cv2
+import numpy as np
+import paddle
+from PIL import Image
+
+from ppdiffusers import (
+    AutoencoderKL,
+    ControlNetModel,
+    StableDiffusionXLControlNetPipeline,
+)
+from ppdiffusers.utils import load_image
+
+prompt = "aerial view, a futuristic research complex in a bright foggy jungle, hard lighting"
+negative_prompt = "low quality, bad quality, sketches"
+
+image = load_image(
+    "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png"
+)
+
+controlnet_conditioning_scale = 0.5  # recommended for good generalization
+
+controlnet = ControlNetModel.from_pretrained("diffusers/controlnet-canny-sdxl-1.0", paddle_dtype=paddle.float16)
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", paddle_dtype=paddle.float16)
+pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    controlnet=controlnet,
+    vae=vae,
+    paddle_dtype=paddle.float16,
+)
+
+image = np.array(image)
+image = cv2.Canny(image, 100, 200)
+image = image[:, :, None]
+image = np.concatenate([image, image, image], axis=2)
+image = Image.fromarray(image)
+
+images = pipe(
+    prompt,
+    negative_prompt=negative_prompt,
+    image=image,
+    controlnet_conditioning_scale=controlnet_conditioning_scale,
+).images
+
+images[0].save("text_to_image_generation-stable_diffusion_xl_controlnet_result.png")
