da034559118911709d62b5234e022d41eee88f59
CrazyBoyM <ai-lab@foxmail.com>
2023-11-28 04:56:06 +0000

    fix semantic_diffusion

--- ppdiffusers/tests/pipelines/semantic_stable_diffusion/test_semantic_diffusion.py
+++ ppdiffusers/tests/pipelines/semantic_stable_diffusion/test_semantic_diffusion.py
@@ -443,69 +443,70 @@ class SemanticDiffusionPipelineIntegrationTests(unittest.TestCase):
         assert image.shape == (1, 512, 512, 3)
         assert np.abs(image_slice.flatten() - expected_slice).max() < 0.02
 
-    # def test_guidance_fp16(self):
-    #     pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", paddle_dtype=paddle.float16)
-    #     pipe.set_progress_bar_config(disable=None)
-    #     prompt = "a photo of a cat"
-    #     edit = {
-    #         "editing_prompt": ["sunglasses"],
-    #         "reverse_editing_direction": [False],
-    #         "edit_warmup_steps": 10,
-    #         "edit_guidance_scale": 6,
-    #         "edit_threshold": 0.95,
-    #         "edit_momentum_scale": 0.5,
-    #         "edit_mom_beta": 0.6,
-    #     }
-    #     seed = 3
-    #     guidance_scale = 7
-    #     generator = paddle.Generator().manual_seed(seed)
-    #     output = pipe(
-    #         [prompt],
-    #         generator=generator,
-    #         guidance_scale=guidance_scale,
-    #         num_inference_steps=50,
-    #         output_type="np",
-    #         width=512,
-    #         height=512,
-    #     )
-    #     image = output.images
-    #     image_slice = image[0, -3:, -3:, -1]
-    #     expected_slice = [
-    #         0.34887695,
-    #         0.3876953,
-    #         0.375,
-    #         0.34423828,
-    #         0.3581543,
-    #         0.35717773,
-    #         0.3383789,
-    #         0.34570312,
-    #         0.359375,
-    #     ]
-    #     assert image.shape == (1, 512, 512, 3)
-    #     assert np.abs(image_slice.flatten() - expected_slice).max() < 0.02
-    #     generator = paddle.Generator().manual_seed(seed)
-    #     output = pipe(
-    #         [prompt],
-    #         generator=generator,
-    #         guidance_scale=guidance_scale,
-    #         num_inference_steps=50,
-    #         output_type="np",
-    #         width=512,
-    #         height=512,
-    #         **edit,
-    #     )
-    #     image = output.images
-    #     image_slice = image[0, -3:, -3:, -1]
-    #     expected_slice = [
-    #         0.42285156,
-    #         0.36914062,
-    #         0.29077148,
-    #         0.42041016,
-    #         0.41918945,
-    #         0.35498047,
-    #         0.3618164,
-    #         0.4423828,
-    #         0.43115234,
-    #     ]
-    #     assert image.shape == (1, 512, 512, 3)
-    #     assert np.abs(image_slice.flatten() - expected_slice).max() < 0.02
+    def test_guidance_fp16(self):
+        # 这里只有fp16精度下能够通过
+        pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", paddle_dtype=paddle.float32)
+        pipe.set_progress_bar_config(disable=None)
+        prompt = "a photo of a cat"
+        edit = {
+            "editing_prompt": ["sunglasses"],
+            "reverse_editing_direction": [False],
+            "edit_warmup_steps": 10,
+            "edit_guidance_scale": 6,
+            "edit_threshold": 0.95,
+            "edit_momentum_scale": 0.5,
+            "edit_mom_beta": 0.6,
+        }
+        seed = 3
+        guidance_scale = 7
+        generator = paddle.Generator().manual_seed(seed)
+        output = pipe(
+            [prompt],
+            generator=generator,
+            guidance_scale=guidance_scale,
+            num_inference_steps=50,
+            output_type="np",
+            width=512,
+            height=512,
+        )
+        image = output.images
+        image_slice = image[0, -3:, -3:, -1]
+        expected_slice = [
+            0.34887695,
+            0.3876953,
+            0.375,
+            0.34423828,
+            0.3581543,
+            0.35717773,
+            0.3383789,
+            0.34570312,
+            0.359375,
+        ]
+        assert image.shape == (1, 512, 512, 3)
+        assert np.abs(image_slice.flatten() - expected_slice).max() < 0.02
+        generator = paddle.Generator().manual_seed(seed)
+        output = pipe(
+            [prompt],
+            generator=generator,
+            guidance_scale=guidance_scale,
+            num_inference_steps=50,
+            output_type="np",
+            width=512,
+            height=512,
+            **edit,
+        )
+        image = output.images
+        image_slice = image[0, -3:, -3:, -1]
+        expected_slice = [
+            0.42285156,
+            0.36914062,
+            0.29077148,
+            0.42041016,
+            0.41918945,
+            0.35498047,
+            0.3618164,
+            0.4423828,
+            0.43115234,
+        ]
+        assert image.shape == (1, 512, 512, 3)
+        assert np.abs(image_slice.flatten() - expected_slice).max() < 0.02
