2bea09acc2d3394f0404c48eed708dc0753b80d4
co63oc <co63@163.com>
2023-12-05 14:04:31 +0800

    Fix

--- ppdiffusers/ppdiffusers/utils/paddle_utils.py
+++ ppdiffusers/ppdiffusers/utils/paddle_utils.py
@@ -243,19 +243,19 @@ def fourier_filter(x_in, threshold, scale):
         x = x.to(dtype=paddle.float32)
 
     # FFT
-    x_freq = fftn(x, dim=(-2, -1))
-    x_freq = fftshift(x_freq, dim=(-2, -1))
+    x_freq = fftn(x, axes=(-2, -1))
+    x_freq = fftshift(x_freq, axes=(-2, -1))
 
     B, C, H, W = x_freq.shape
-    mask = paddle.ones((B, C, H, W), device=x.device)
+    mask = paddle.ones((B, C, H, W))
 
     crow, ccol = H // 2, W // 2
     mask[..., crow - threshold : crow + threshold, ccol - threshold : ccol + threshold] = scale
     x_freq = x_freq * mask
 
     # IFFT
-    x_freq = ifftshift(x_freq, dim=(-2, -1))
-    x_filtered = ifftn(x_freq, dim=(-2, -1)).real
+    x_freq = ifftshift(x_freq, axes=(-2, -1))
+    x_filtered = ifftn(x_freq, axes=(-2, -1)).real
 
     return x_filtered.to(dtype=x_in.dtype)
 
