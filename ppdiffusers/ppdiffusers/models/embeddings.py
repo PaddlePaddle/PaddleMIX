6891a2bfb29cb86e4f06e56f3a04aa9ebb693e1d
co63oc <co63@163.com>
2023-12-07 09:42:46 +0800

    Fix

--- ppdiffusers/ppdiffusers/models/embeddings.py
+++ ppdiffusers/ppdiffusers/models/embeddings.py
@@ -309,9 +309,13 @@ class SinusoidalPositionalEmbedding(nn.Layer):
         super().__init__()
         position = paddle.arange(max_seq_length).unsqueeze(1)
         div_term = paddle.exp(paddle.arange(0, embed_dim, 2) * (-math.log(10000.0) / embed_dim))
-        pe = paddle.zeros(1, max_seq_length, embed_dim)
-        pe[0, :, 0::2] = paddle.sin(position * div_term)
-        pe[0, :, 1::2] = paddle.cos(position * div_term)
+        pe = paddle.zeros([1, max_seq_length, embed_dim])
+        pe[0, :, 0::2] = paddle.sin(
+            paddle.to_tensor(position * div_term, dtype=paddle.get_default_dtype())
+        )  # paddle: sin not support int64, convert to float32
+        pe[0, :, 1::2] = paddle.cos(
+            paddle.to_tensor(position * div_term, dtype=paddle.get_default_dtype())
+        )  # paddle: cos not support int64, convert to float32
         self.register_buffer("pe", pe)
 
     def forward(self, x):
