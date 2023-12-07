a673b5f187b9328852c002c38e002146eb502b2e
co63oc <co63@163.com>
2023-12-07 11:33:03 +0800

    Fix

--- ppdiffusers/ppdiffusers/models/unet_motion_model.py
+++ ppdiffusers/ppdiffusers/models/unet_motion_model.py
@@ -763,7 +763,7 @@ class UNetMotionModel(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin):
         # timesteps does not contain any weights and will always return f32 tensors
         # but time_embedding might actually be running in fp16. so we need to cast here.
         # there might be better ways to encapsulate this.
-        t_emb = t_emb.to(dtype=self.dtype)
+        t_emb = t_emb.cast(dtype=self.dtype)  # paddle develop has .to()
 
         emb = self.time_embedding(t_emb, timestep_cond)
         emb = emb.repeat_interleave(repeats=num_frames, axis=0)
--- ppdiffusers/tests/pipelines/stable_diffusion/test_stable_diffusion.py
+++ ppdiffusers/tests/pipelines/stable_diffusion/test_stable_diffusion.py
@@ -37,7 +37,7 @@ from ppdiffusers import (
 from ppdiffusers.utils import nightly, slow
 from ppdiffusers.utils.testing_utils import CaptureLogger, require_paddle_gpu
 
-from ...lora.test_lora_layers import create_lora_layers
+from ...lora.test_lora_layers_old_backend import create_lora_layers
 from ..pipeline_params import TEXT_TO_IMAGE_BATCH_PARAMS, TEXT_TO_IMAGE_PARAMS
 from ..test_pipelines_common import PipelineTesterMixin
 
