16f83a1d5b6ab54e0cc12923a6998b2d55c57bb5
liujun121533 <963571946@qq.com>
2023-12-15 23:51:20 +0800

    first commit

---
+++ tests/appflow/test_autolabel.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import unittest
+
+import numpy as np
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.."))
+from paddlemix.appflow import Appflow
+from ppdiffusers.utils import load_image, load_numpy
+from tests.testing_utils import _run_slow_test
+
+
+class OpenSetDetSamAppSlowTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.url = (
+            "https://paddlenlp.bj.bcebos.com/models/community/CompVis/stable-diffusion-v1-4/overture-creations.png"
+        )
+        cls.expected_image = load_numpy(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_autolabel_mask.npy")
+        )
+        cls.expected_labels = ['dog', 'bench', 'field']
+        cls.expected_boxes = np.array([[174, 116, 312, 467],
+            [ 32, 223, 464, 491],
+            [  0, 141, 511, 511]])
+    
+    def test_autolable(self):
+            
+            self.task = Appflow(
+                app="auto_label",
+                models=["paddlemix/blip2-caption-opt2.7b", "GroundingDino/groundingdino-swint-ogc", "Sam/SamVitH-1024"],
+            )
+            image_pil = load_image(self.url)
+            blip2_prompt = "describe the image"
+            result = self.task(image=image_pil, blip2_prompt=blip2_prompt)
+
+            # check labels
+            # res_labels like:  ['dog(0.72)', 'bench(0.63)', 'field(0.49)']
+            res_labels_name = [label.split('(')[0] for label in result['labels']]
+            res_label_expected_label_map = []
+            for expected_l in self.expected_labels:
+                self.assertIn(expected_l, res_labels_name)
+            
+            for label in res_labels_name:
+                res_label_expected_label_map.append(self.expected_labels.index(label))
+            assert len(result['boxes']) == len(self.expected_boxes), "Error box num"
+            for i, box in enumerate(result['boxes']):
+                avg_diff = np.abs(self.expected_boxes[res_label_expected_label_map[i]] - box).mean()
+                assert avg_diff < 5, f"Error bbox deviates {avg_diff} pixels on average"
+
+            avg_diff = np.abs(
+                result["seg_masks"][0].cpu().numpy().astype(int) - self.expected_image.astype(int)
+            ).mean()
+            assert avg_diff < 10, f"Error image deviates {avg_diff} pixels on average"
+
+
+if __name__ == "__main__":
+
+    unittest.main()
---
+++ tests/models/test_groundingdino.py
@@ -0,0 +1,163 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import sys
+from paddlemix.models.groundingdino.configuration import GroundingDinoConfig
+from paddlemix.models.groundingdino.modeling import GroundingDinoModel
+from paddlemix.processors.groundingdino_processing import GroudingDinoProcessor
+
+
+from paddlemix.models.sam.modeling import SamModel
+from paddlemix.models.sam.configuration import SamConfig
+
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.."))
+
+from PIL import Image
+from paddlemix.processors.sam_processing import SamProcessor
+import requests
+
+
+import inspect
+import tempfile
+import unittest
+
+import numpy as np
+import paddle
+import paddle.nn as nn
+from paddlenlp.transformers.opt.configuration import OPTConfig
+
+from paddlemix.models.blip2 import (
+    Blip2Config,
+    Blip2ForConditionalGeneration,
+    Blip2QFormerConfig,
+    Blip2VisionConfig,
+)
+from paddlemix.models.blip2.eva_vit import VisionTransformer
+from paddlemix.models.blip2.modeling import BLIP_2_PRETRAINED_MODEL_ARCHIVE_LIST
+from paddlemix.models.blip2.Qformer import BertLMHeadModel
+from tests.models.test_configuration_common import ConfigTester
+from tests.models.test_modeling_common import (
+    ModelTesterMixin,
+    floats_tensor,
+    ids_tensor,
+    random_attention_mask,
+)
+from tests.testing_utils import slow
+
+
+
+class GroundingDinoModelTester:
+    def __init__(self):
+        pass
+
+class GroundingDinoModelTest(ModelTesterMixin, unittest.TestCase):
+    all_model_classes = (GroundingDinoModel,)
+    fx_compatible = False
+    test_head_masking = False
+    test_pruning = False
+    test_resize_embeddings = False
+    test_attention_outputs = False
+    use_test_model_name_list = False
+    use_test_inputs_embeds: bool = False
+
+    def setUp(self):
+        pass
+        # self.model_tester = SamModelTester(self)
+
+    def test_for_conditional_generation(self):
+        config_and_inputs = self.model_tester.prepare_config_and_inputs()
+        self.model_tester.create_and_check_for_conditional_generation(*config_and_inputs)
+
+    @unittest.skip(reason="Hidden_states is tested in individual model tests")
+    def test_hidden_states_output(self):
+        pass
+
+    def test_determinism(self):
+        # config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
+        url = '/home/aistudio/work/PaddleMIX/paddlemix/examples/Sam/overture-creations.png'
+        if os.path.isfile(url):
+            # read image
+            image_pil = Image.open(url).convert("RGB")
+        else:
+            image_pil = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+    
+        processor = GroudingDinoProcessor.from_pretrained('GroundingDino/groundingdino-swint-ogc')
+
+        image_tensor, mask, tokenized_out = processor(images=image_pil, text='cat')
+
+        def check_determinism(first, second):
+            out_1 = first.numpy()
+            out_2 = second.numpy()
+            out_1 = out_1[~np.isnan(out_1) & ~np.isinf(out_1)]
+            out_2 = out_2[~np.isnan(out_2) & ~np.isinf(out_2)]
+            max_diff = np.amax(np.abs(out_1 - out_2))
+            self.assertLessEqual(max_diff, 1e-5)
+
+
+        config = GroundingDinoConfig()
+        
+        inputs_dict = {
+            'x': image_tensor,
+            'm': mask,
+            'input_ids': tokenized_out["input_ids"],
+            'attention_mask': tokenized_out["attention_mask"],
+            'text_self_attention_masks': tokenized_out["text_self_attention_masks"],
+            'position_ids': tokenized_out["position_ids"],
+        }
+        for model_class in self.all_model_classes:
+            model = self._make_model_instance(config, model_class)
+            
+            model.eval()
+            with paddle.no_grad():
+                input = self._prepare_for_class(inputs_dict, model_class)
+                first = model(**input)
+                second = model(**input)
+
+            for k in ['pred_logits', 'pred_boxes']:
+                if isinstance(first[k], tuple) and isinstance(second[k], tuple):
+                    for tensor1, tensor2 in zip(first[k], second[k]):
+                        check_determinism(tensor1, tensor2)
+                else:
+                    check_determinism(first[k], second[k])
+
+    def test_forward_signature(self):
+        config = GroundingDinoConfig()
+        for model_class in self.all_model_classes:
+            model = model_class(config)
+            signature = inspect.signature(model.forward)
+            # signature.parameters is an OrderedDict => so arg_names order is deterministic
+            arg_names = [*signature.parameters.keys()]
+
+            expected_arg_names = ["x", "m", "input_ids", "attention_mask", "text_self_attention_masks", 
+                                  "position_ids", "targets"]
+            self.assertListEqual(arg_names[:7], expected_arg_names)
+
+    # todo: test_loadSamConfig
+    def test_dinoconfig_from_pretrained(self):
+        config = GroundingDinoConfig.from_pretrained("GroundingDino/groundingdino-swint-ogc")
+        self.assertIsNotNone(config)
+
+
+    #todo @slow
+    def test_model_from_pretrained(self):
+        model = GroundingDinoModel.from_pretrained('GroundingDino/groundingdino-swint-ogc')
+        self.assertIsNotNone(model)
+
+    def test_save_load(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
