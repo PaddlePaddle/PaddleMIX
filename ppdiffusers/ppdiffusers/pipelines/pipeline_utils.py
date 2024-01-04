925008b39cf325af5503c64cade7616563484f7b
yujun <573009727@qq.com>
2024-01-04 10:47:43 +0800

    添加

--- ppdiffusers/ppdiffusers/pipelines/pipeline_utils.py
+++ ppdiffusers/ppdiffusers/pipelines/pipeline_utils.py
@@ -1387,3 +1387,13 @@ class DiffusionPipeline(ConfigMixin):
         if not hasattr(self, "unet"):
             raise ValueError("The pipeline must have `unet` for using FreeU.")
         self.unet.disable_freeu()
+
+    def maybe_free_model_hooks(self):
+        r"""
+        Function that offloads all components, removes all model hooks that were added when using
+        `enable_model_cpu_offload` and then applies them again. In case the model has not been offloaded this function
+        is a no-op. Make sure to add this function to the end of the `__call__` function of your pipeline so that it
+        functions correctly when applying enable_model_cpu_offload.
+        """
+        # do nothing
+        return
