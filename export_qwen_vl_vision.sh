5454a22a2d1d21ecba8602970084f4d1ac8d76a6
wucc <77946882+DanGuge@users.noreply.github.com>
2024-01-08 22:43:48 +0800

    remove scripts

--- export_qwen_vl_vision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=0
-export PYTHONPATH=/path/to/PaddleNLP/:/path/to/PaddleMIX
-
-python deploy/qwen_vl/export_image_encoder.py \
-    --model_name_or_path "qwen-vl/qwen-vl-7b" \
--- run_qwen_vl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=0
-export PYTHONPATH=/path/to/PaddleNLP/:/path/to/PaddleMIX
-
-python deploy/qwen_vl/run_static_predict.py \
-    --first_model_path "/path/to/checkpoints/encode_image_fp16/vision" \
-    --second_model_path "/path/to/checkpoints/encode_text_fp16/qwen" \
-    --qwen_vl_config_path "qwen-vl/qwen-vl-7b" \
