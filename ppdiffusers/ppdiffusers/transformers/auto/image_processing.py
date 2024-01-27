dbc4e198bfa245c524f4b5bb5f37f22048c3cbae
yujun <573009727@qq.com>
2024-01-27 11:27:51 +0800

    兼容2.7.3版本

--- ppdiffusers/ppdiffusers/transformers/auto/image_processing.py
+++ ppdiffusers/ppdiffusers/transformers/auto/image_processing.py
@@ -13,106 +13,91 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import inspect
-import io
-import json
-import os
-from collections import OrderedDict, defaultdict
+try:
+    from paddlenlp.transformers.auto.image_processing import (
+        AutoImageProcessor as PPNLPAutoImageProcessor,
+    )
+    from paddlenlp.transformers.auto.image_processing import IMAGE_PROCESSOR_MAPPING_NAMES
 
-from paddlenlp.transformers.auto.image_processing import (
-    AutoImageProcessor as PPNLPAutoImageProcessor,
-)
-from paddlenlp.transformers.image_processing_utils import BaseImageProcessor
-from paddlenlp.utils.import_utils import import_module
 
-from ...utils import logging
+    import inspect
+    import io
+    import json
+    import os
+    from collections import OrderedDict, defaultdict
 
-logger = logging.get_logger(__name__)
+    from paddlenlp.transformers.image_processing_utils import BaseImageProcessor
+    from paddlenlp.utils.import_utils import import_module
 
-__all__ = [
-    "AutoImageProcessor",
-]
-from paddlenlp.transformers.auto.image_processing import IMAGE_PROCESSOR_MAPPING_NAMES
+    from ...utils import logging
 
-NEW_IMAGE_PROCESSOR_MAPPING_NAMES = OrderedDict(
-    [
-        ("CLIPImageProcessor", "clip"),
+    logger = logging.get_logger(__name__)
+
+    __all__ = [
+        "AutoImageProcessor",
     ]
-)
-IMAGE_PROCESSOR_MAPPING_NAMES.update(NEW_IMAGE_PROCESSOR_MAPPING_NAMES)
+
+    NEW_IMAGE_PROCESSOR_MAPPING_NAMES = OrderedDict(
+        [
+            ("CLIPImageProcessor", "clip"),
+        ]
+    )
+    IMAGE_PROCESSOR_MAPPING_NAMES.update(NEW_IMAGE_PROCESSOR_MAPPING_NAMES)
 
 
-def get_configurations():
-    """load the configurations of PretrainedConfig mapping: {<model-name>: [<class-name>, <class-name>, ...], }
+    def get_configurations():
+        """load the configurations of PretrainedConfig mapping: {<model-name>: [<class-name>, <class-name>, ...], }
 
-    Returns:
-        dict[str, str]: the mapping of model-name to model-classes
-    """
-    # 1. search the subdir<model-name> to find model-names
-    ppdiffusers_transformers_dir = os.path.dirname(os.path.dirname(__file__))
-    exclude_models = ["auto"]
+        Returns:
+            dict[str, str]: the mapping of model-name to model-classes
+        """
+        # 1. search the subdir<model-name> to find model-names
+        ppdiffusers_transformers_dir = os.path.dirname(os.path.dirname(__file__))
+        exclude_models = ["auto"]
 
-    mappings = defaultdict(list)
-    for model_name in os.listdir(ppdiffusers_transformers_dir):
-        if model_name in exclude_models:
-            continue
+        mappings = defaultdict(list)
+        for model_name in os.listdir(ppdiffusers_transformers_dir):
+            if model_name in exclude_models:
+                continue
 
-        model_dir = os.path.join(ppdiffusers_transformers_dir, model_name)
-        if not os.path.isdir(model_dir):
-            continue
+            model_dir = os.path.join(ppdiffusers_transformers_dir, model_name)
+            if not os.path.isdir(model_dir):
+                continue
 
-        # 2. find the `configuration.py` file as the identifier of PretrainedConfig class
-        image_processing_path = os.path.join(model_dir, "image_processing.py")
-        if not os.path.exists(image_processing_path):
-            continue
+            # 2. find the `configuration.py` file as the identifier of PretrainedConfig class
+            image_processing_path = os.path.join(model_dir, "image_processing.py")
+            if not os.path.exists(image_processing_path):
+                continue
 
-        for package in ["paddlenlp", "ppdiffusers"]:
-            image_processing_module = import_module(f"{package}.transformers.{model_name}.image_processing")
-            for key in dir(image_processing_module):
-                value = getattr(image_processing_module, key)
-                if inspect.isclass(value) and issubclass(value, BaseImageProcessor):
-                    mappings[model_name].append(value)
+            for package in ["paddlenlp", "ppdiffusers"]:
+                image_processing_module = import_module(f"{package}.transformers.{model_name}.image_processing")
+                for key in dir(image_processing_module):
+                    value = getattr(image_processing_module, key)
+                    if inspect.isclass(value) and issubclass(value, BaseImageProcessor):
+                        mappings[model_name].append(value)
 
-    return mappings
+        return mappings
 
 
-class AutoImageProcessor(PPNLPAutoImageProcessor):
-    MAPPING_NAMES = get_configurations()
-    _processor_mapping = MAPPING_NAMES
-    _name_mapping = IMAGE_PROCESSOR_MAPPING_NAMES
+    class AutoImageProcessor(PPNLPAutoImageProcessor):
+        MAPPING_NAMES = get_configurations()
+        _processor_mapping = MAPPING_NAMES
+        _name_mapping = IMAGE_PROCESSOR_MAPPING_NAMES
 
-    @classmethod
-    def _get_image_processor_class_from_config(cls, pretrained_model_name_or_path, config_file_path):
-        processor_class = None
-        with io.open(config_file_path, encoding="utf-8") as f:
-            init_kwargs = json.load(f)
-        # class name corresponds to this configuration
-        init_class = init_kwargs.pop("init_class", None)
-        if init_class is None:
-            init_class = init_kwargs.pop("image_processor_type", None)
+        @classmethod
+        def _get_image_processor_class_from_config(cls, pretrained_model_name_or_path, config_file_path):
+            processor_class = None
+            with io.open(config_file_path, encoding="utf-8") as f:
+                init_kwargs = json.load(f)
+            # class name corresponds to this configuration
+            init_class = init_kwargs.pop("init_class", None)
+            if init_class is None:
+                init_class = init_kwargs.pop("image_processor_type", None)
 
-        if init_class:
-            # replace old name to new name
-            init_class = init_class.replace("FeatureExtractor", "ImageProcessor")
-            try:
-                class_name = cls._name_mapping[init_class]
-                for package in ["ppdiffusers", "paddlenlp"]:
-                    import_class = import_module(f"{package}.transformers.{class_name}.image_processing")
-                    if import_class is not None:
-                        break
-                if import_class is None:
-                    raise ImportError(f"Cannot find the {class_name} from paddlenlp or ppdiffusers.")
-                processor_class = getattr(import_class, init_class)
-                return processor_class
-            except Exception:
-                init_class = None
-
-        # If no `init_class`, we use pattern recognition to recognize the processor class.
-        if init_class is None:
-            logger.info("We use pattern recognition to recognize the processor class.")
-            for key, pattern in cls._name_mapping.items():
-                if pattern in pretrained_model_name_or_path.lower():
-                    init_class = key
+            if init_class:
+                # replace old name to new name
+                init_class = init_class.replace("FeatureExtractor", "ImageProcessor")
+                try:
                     class_name = cls._name_mapping[init_class]
                     for package in ["ppdiffusers", "paddlenlp"]:
                         import_class = import_module(f"{package}.transformers.{class_name}.image_processing")
@@ -121,7 +106,27 @@ class AutoImageProcessor(PPNLPAutoImageProcessor):
                     if import_class is None:
                         raise ImportError(f"Cannot find the {class_name} from paddlenlp or ppdiffusers.")
                     processor_class = getattr(import_class, init_class)
-                    break
-        if processor_class is None:
-            raise ImportError("Cannot find the image_processing from paddlenlp or ppdiffusers.")
-        return processor_class
+                    return processor_class
+                except Exception:
+                    init_class = None
+
+            # If no `init_class`, we use pattern recognition to recognize the processor class.
+            if init_class is None:
+                logger.info("We use pattern recognition to recognize the processor class.")
+                for key, pattern in cls._name_mapping.items():
+                    if pattern in pretrained_model_name_or_path.lower():
+                        init_class = key
+                        class_name = cls._name_mapping[init_class]
+                        for package in ["ppdiffusers", "paddlenlp"]:
+                            import_class = import_module(f"{package}.transformers.{class_name}.image_processing")
+                            if import_class is not None:
+                                break
+                        if import_class is None:
+                            raise ImportError(f"Cannot find the {class_name} from paddlenlp or ppdiffusers.")
+                        processor_class = getattr(import_class, init_class)
+                        break
+            if processor_class is None:
+                raise ImportError("Cannot find the image_processing from paddlenlp or ppdiffusers.")
+            return processor_class
+except ImportError:
+    pass
