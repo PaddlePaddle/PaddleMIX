b2a2db8cc03f37f7fb4b8df76078fc23b6aee153
yujun <573009727@qq.com>
2024-01-30 14:55:21 +0800

    do_gradient_checkpointing

--- ppdiffusers/tests/examples_test/train.sh
+++ ppdiffusers/tests/examples_test/train.sh
@@ -228,43 +228,43 @@ export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
 export VAE_NAME="madebyollin/sdxl-vae-fp16-fix"
 export DATASET_NAME="lambdalabs/pokemon-blip-captions"
 RESOLUTION=768
-export OUTPUT_DIR="sd-pokemon-model-sdxl"
+# export OUTPUT_DIR="sd-pokemon-model-sdxl"
 
-python -u train_text_to_image_sdxl.py \
-  --pretrained_model_name_or_path=$MODEL_NAME \
-  --pretrained_vae_model_name_or_path=$VAE_NAME \
-  --dataset_name=$DATASET_NAME \
-  --enable_xformers_memory_efficient_attention \
-  --resolution=${RESOLUTION} --center_crop --random_flip \
-  --proportion_empty_prompts=0.2 \
-  --train_batch_size=1 \
-  --gradient_accumulation_steps=1 --gradient_checkpointing \
-  --max_train_steps=50 \
-  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --mixed_precision="fp16" \
-  --report_to="visualdl" \
-  --validation_prompt="a cute Sundar Pichai creature" --validation_epochs 1 \
-  --checkpointing_steps=20 \
-  --output_dir=${OUTPUT_DIR}
+# python -u train_text_to_image_sdxl.py \
+#   --pretrained_model_name_or_path=$MODEL_NAME \
+#   --pretrained_vae_model_name_or_path=$VAE_NAME \
+#   --dataset_name=$DATASET_NAME \
+#   --enable_xformers_memory_efficient_attention \
+#   --resolution=${RESOLUTION} --center_crop --random_flip \
+#   --proportion_empty_prompts=0.2 \
+#   --train_batch_size=1 \
+#   --gradient_accumulation_steps=1 --gradient_checkpointing \
+#   --max_train_steps=50 \
+#   --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+#   --mixed_precision="fp16" \
+#   --report_to="visualdl" \
+#   --validation_prompt="a cute Sundar Pichai creature" --validation_epochs 1 \
+#   --checkpointing_steps=20 \
+#   --output_dir=${OUTPUT_DIR}
 
 
-export OUTPUT_DIR="sd-pokemon-model-sdxl-duoka"
-python -u -m paddle.distributed.launch --gpus "0,1" train_text_to_image_sdxl.py \
-  --pretrained_model_name_or_path=$MODEL_NAME \
-  --pretrained_vae_model_name_or_path=$VAE_NAME \
-  --dataset_name=$DATASET_NAME \
-  --enable_xformers_memory_efficient_attention \
-  --resolution=${RESOLUTION} --center_crop --random_flip \
-  --proportion_empty_prompts=0.2 \
-  --train_batch_size=1 \
-  --gradient_accumulation_steps=1 --gradient_checkpointing \
-  --max_train_steps=50 \
-  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --mixed_precision="fp16" \
-  --report_to="visualdl" \
-  --validation_prompt="a cute Sundar Pichai creature" --validation_epochs 1 \
-  --checkpointing_steps=20 \
-  --output_dir=${OUTPUT_DIR}
+# export OUTPUT_DIR="sd-pokemon-model-sdxl-duoka"
+# python -u -m paddle.distributed.launch --gpus "0,1" train_text_to_image_sdxl.py \
+#   --pretrained_model_name_or_path=$MODEL_NAME \
+#   --pretrained_vae_model_name_or_path=$VAE_NAME \
+#   --dataset_name=$DATASET_NAME \
+#   --enable_xformers_memory_efficient_attention \
+#   --resolution=${RESOLUTION} --center_crop --random_flip \
+#   --proportion_empty_prompts=0.2 \
+#   --train_batch_size=1 \
+#   --gradient_accumulation_steps=1 --gradient_checkpointing \
+#   --max_train_steps=50 \
+#   --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+#   --mixed_precision="fp16" \
+#   --report_to="visualdl" \
+#   --validation_prompt="a cute Sundar Pichai creature" --validation_epochs 1 \
+#   --checkpointing_steps=20 \
+#   --output_dir=${OUTPUT_DIR}
 
 
 export OUTPUT_DIR="sd-pokemon-model-lora-sdxl"
