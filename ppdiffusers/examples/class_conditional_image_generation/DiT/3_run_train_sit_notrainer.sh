4e52feb2a96e6baf41326c9a4147a20b3450b333
nemonameless@qq.com@github.com <nemonameless@qq.com>
2024-02-26 01:55:45 +0000

    support DiT and SiT class conditional image generation

---
+++ ppdiffusers/examples/class_conditional_image_generation/DiT/3_run_train_sit_notrainer.sh
@@ -0,0 +1,10 @@
+export FLAGS_embedding_deterministic=1
+export FLAGS_cudnn_deterministic=1
+export NVIDIA_TF32_OVERRIDE=0
+export NCCL_ALGO=Tree
+
+python -u -m paddle.distributed.launch --gpus "0,1,2,3,4,5,6,7" \
+    train_image_generation_notrainer.py \
+    --config_file config/SiT_XL_patch2.json \
+    --feature_path ./data/fastdit_imagenet256 \
+    --global_batch_size 16
