4cfd103936daea709325666f5840e6604bb07d9a
coco <1228759711@qq.com>
2024-03-05 12:59:30 +0800

    rm torch relative print, rm DEVICE, rename torch_type to data_type

--- paddlemix/examples/cogagent/chat_demo.py
+++ paddlemix/examples/cogagent/chat_demo.py
@@ -33,23 +33,19 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--from_pretrained", type=str, default="THUDM/cogagent-chat-hf", help="pretrained ckpt")
 parser.add_argument("--local_tokenizer", type=str, default="lmsys/vicuna-7b-v1.5")
-parser.add_argument("--local_tokenizer", type=str, default="lmsys/vicuna-7b-v1.5")
 args = parser.parse_args()
 MODEL_PATH = args.from_pretrained
 TOKENIZER_PATH = args.local_tokenizer
-DEVICE = "gpu" if paddle.device.cuda.device_count() >= 1 else "cpu"
 
 tokenizer = AutoTokenizerMIX.from_pretrained(TOKENIZER_PATH)
 
-torch_type = "float32"
-print("========Use torch type as:{} with device:{}========\n\n".format(torch_type, DEVICE))
-paddle.set_device(DEVICE)
+data_type = "float32"
 
 model = AutoModelMIX.from_pretrained(
     MODEL_PATH,
-    dtype=torch_type,
+    dtype=data_type,
     low_cpu_mem_usage=False,
-).to(DEVICE)
+)
 model.eval()
 
 text_only_template = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {} ASSISTANT:"
@@ -84,13 +80,13 @@ while True:
                 tokenizer, query=query, history=history, images=[image]
             )
         inputs = {
-            "input_ids": input_by_model["input_ids"].unsqueeze(axis=0).to(DEVICE),
-            "token_type_ids": input_by_model["token_type_ids"].unsqueeze(axis=0).to(DEVICE),
-            "attention_mask": input_by_model["attention_mask"].unsqueeze(axis=0).to(DEVICE),
-            "images": [[input_by_model["images"][0].to(DEVICE).to(torch_type)]] if image is not None else None,
+            "input_ids": input_by_model["input_ids"].unsqueeze(axis=0),
+            "token_type_ids": input_by_model["token_type_ids"].unsqueeze(axis=0),
+            "attention_mask": input_by_model["attention_mask"].unsqueeze(axis=0),
+            "images": [[input_by_model["images"][0].to(data_type)]] if image is not None else None,
         }
         if "cross_images" in input_by_model and input_by_model["cross_images"]:
-            inputs["cross_images"] = [[input_by_model["cross_images"][0].to(DEVICE).to(torch_type)]]
+            inputs["cross_images"] = [[input_by_model["cross_images"][0].to(data_type)]]
         gen_kwargs = {"max_new_tokens": 2048, "do_sample": False}
         with paddle.no_grad():
             outputs, _ = model.generate(**inputs, **gen_kwargs)
