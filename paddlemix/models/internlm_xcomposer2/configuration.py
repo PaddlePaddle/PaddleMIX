9b05582aac974f5b40c10e2f849cc4a9b66eaca8
coco <1228759711@qq.com>
2024-04-20 03:32:25 +0800

    precommit configuration.py

--- paddlemix/models/internlm_xcomposer2/configuration.py
+++ paddlemix/models/internlm_xcomposer2/configuration.py
@@ -1,12 +1,24 @@
-import paddle
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import paddlenlp.transformers as transformers
 
 """InternLM model configuration."""
 INTERNLM_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
 
 
-class InternLMXcomposer2Config(transformers.configuration_utils.
-    PretrainedConfig):
+class InternLMXcomposer2Config(transformers.configuration_utils.PretrainedConfig):
     """
     This is the configuration class to store the configuration of a [`InternLMModel`]. It is used to instantiate
     an InternLM model according to the specified arguments, defining the model architecture. Instantiating a
@@ -64,16 +76,33 @@ class InternLMXcomposer2Config(transformers.configuration_utils.
     >>> # Accessing the model configuration
     >>> configuration = model.config
     ```"""
-    model_type = 'internlm'
-    _auto_class = 'AutoConfig'
 
-    def __init__(self, vocab_size=103168, hidden_size=4096,
-        intermediate_size=11008, num_hidden_layers=32, num_attention_heads=
-        32, num_key_value_heads=None, hidden_act='silu',
-        max_position_embeddings=2048, initializer_range=0.02, rms_norm_eps=
-        1e-06, use_cache=True, pad_token_id=0, bos_token_id=1, eos_token_id
-        =2, tie_word_embeddings=False, bias=True, rope_theta=10000,
-        rope_scaling=None, attn_implementation='eager', **kwargs):
+    model_type = "internlm"
+    _auto_class = "AutoConfig"
+
+    def __init__(
+        self,
+        vocab_size=103168,
+        hidden_size=4096,
+        intermediate_size=11008,
+        num_hidden_layers=32,
+        num_attention_heads=32,
+        num_key_value_heads=None,
+        hidden_act="silu",
+        max_position_embeddings=2048,
+        initializer_range=0.02,
+        rms_norm_eps=1e-06,
+        use_cache=True,
+        pad_token_id=0,
+        bos_token_id=1,
+        eos_token_id=2,
+        tie_word_embeddings=False,
+        bias=True,
+        rope_theta=10000,
+        rope_scaling=None,
+        attn_implementation="eager",
+        **kwargs
+    ):
         self.vocab_size = vocab_size
         self.max_position_embeddings = max_position_embeddings
         self.hidden_size = hidden_size
@@ -93,29 +122,28 @@ class InternLMXcomposer2Config(transformers.configuration_utils.
         self._rope_scaling_validation()
         self.attn_implementation = attn_implementation
         if self.attn_implementation is None:
-            self.attn_implementation = 'eager'
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=
-            bos_token_id, eos_token_id=eos_token_id, tie_word_embeddings=
-            tie_word_embeddings, **kwargs)
+            self.attn_implementation = "eager"
+        super().__init__(
+            pad_token_id=pad_token_id,
+            bos_token_id=bos_token_id,
+            eos_token_id=eos_token_id,
+            tie_word_embeddings=tie_word_embeddings,
+            **kwargs,
+        )
 
     def _rope_scaling_validation(self):
         """Validate the `rope_scaling` configuration."""
         if self.rope_scaling is None:
             return
-        if not isinstance(self.rope_scaling, dict) or len(self.rope_scaling
-            ) != 2:
+        if not isinstance(self.rope_scaling, dict) or len(self.rope_scaling) != 2:
             raise ValueError(
-                f'`rope_scaling` must be a dictionary with with two fields, `type` and `factor`, got {self.rope_scaling}'
-                )
-        rope_scaling_type = self.rope_scaling.get('type', None)
-        rope_scaling_factor = self.rope_scaling.get('factor', None)
-        if rope_scaling_type is None or rope_scaling_type not in ['linear',
-            'dynamic']:
+                f"`rope_scaling` must be a dictionary with with two fields, `type` and `factor`, got {self.rope_scaling}"
+            )
+        rope_scaling_type = self.rope_scaling.get("type", None)
+        rope_scaling_factor = self.rope_scaling.get("factor", None)
+        if rope_scaling_type is None or rope_scaling_type not in ["linear", "dynamic"]:
             raise ValueError(
                 f"`rope_scaling`'s type field must be one of ['linear', 'dynamic'], got {rope_scaling_type}"
-                )
-        if rope_scaling_factor is None or not isinstance(rope_scaling_factor,
-            float) or rope_scaling_factor < 1.0:
-            raise ValueError(
-                f"`rope_scaling`'s factor field must be a float >= 1, got {rope_scaling_factor}"
-                )
+            )
+        if rope_scaling_factor is None or not isinstance(rope_scaling_factor, float) or rope_scaling_factor < 1.0:
+            raise ValueError(f"`rope_scaling`'s factor field must be a float >= 1, got {rope_scaling_factor}")
