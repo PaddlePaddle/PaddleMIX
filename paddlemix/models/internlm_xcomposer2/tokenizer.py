763f6e58d8fd6acbb750636942575a43eabc935f
coco <1228759711@qq.com>
2024-04-20 03:51:33 +0800

    clean tokenizer.py

--- paddlemix/models/internlm_xcomposer2/tokenizer.py
+++ paddlemix/models/internlm_xcomposer2/tokenizer.py
@@ -1,15 +1,32 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Tokenization classes for IntermLM."""
 import os
-import paddle
+import re
 from shutil import copyfile
 from typing import Any, Dict, List, Optional, Tuple
 
 import sentencepiece as spm
-import paddlenlp.transformers as transformers
 from paddlenlp.transformers.tokenizer_utils import PretrainedTokenizer
-
 from paddlenlp.transformers.tokenizer_utils_base import AddedToken
- 
+
+from ppdiffusers.utils import logging
+
+logger = logging.get_logger(__name__)
+
+
 class InternLMXComposer2Tokenizer(PretrainedTokenizer):
     """Construct a InternLM tokenizer. Based on byte-level Byte-Pair-Encoding.
 
@@ -17,25 +34,26 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
         vocab_file (`str`):
             Path to the vocabulary file.
     """
-    resource_files_names = {'vocab_file': 'sentencepiece.bpe.model'}
+
+    resource_files_names = {"vocab_file": "sentencepiece.bpe.model"}
     pretrained_resource_files_map = {}
-    model_input_names = ['input_ids', 'attention_mask']
-    _auto_class = 'AutoTokenizer'
+    model_input_names = ["input_ids", "attention_mask"]
+    _auto_class = "AutoTokenizer"
 
     def __init__(
         self,
         vocab_file,
-        unk_token='<unk>',
-        bos_token='<s>',
-        eos_token='</s>',
-        pad_token='</s>',
-        sp_model_kwargs: Optional[Dict[str, Any]] = None, 
+        unk_token="<unk>",
+        bos_token="<s>",
+        eos_token="</s>",
+        pad_token="</s>",
+        sp_model_kwargs: Optional[Dict[str, Any]] = None,
         add_bos_token=True,
         add_eos_token=False,
         decode_with_prefix_space=False,
         clean_up_tokenization_spaces=False,
         **kwargs,
-        ):
+    ):
         self.sp_model_kwargs = {} if sp_model_kwargs is None else sp_model_kwargs
         self.vocab_file = vocab_file
         self.add_bos_token = add_bos_token
@@ -47,21 +65,18 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
         super().__init__(
             bos_token=bos_token,
             eos_token=eos_token,
-            unk_token=unk_token, 
+            unk_token=unk_token,
             pad_token=pad_token,
-            clean_up_tokenization_spaces=clean_up_tokenization_spaces, 
+            clean_up_tokenization_spaces=clean_up_tokenization_spaces,
             **kwargs,
-            )
+        )
         """ Initialization"""
 
     @property
     def no_prefix_space_tokens(self):
         if self._no_prefix_space_tokens is None:
             vocab = self.convert_ids_to_tokens(list(range(self.vocab_size)))
-            self._no_prefix_space_tokens = {
-                i
-                for i, tok in enumerate(vocab) if not tok.startswith('▁')
-            }
+            self._no_prefix_space_tokens = {i for i, tok in enumerate(vocab) if not tok.startswith("▁")}
         return self._no_prefix_space_tokens
 
     @property
@@ -79,10 +94,7 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
 
     def get_vocab(self):
         """Returns vocab as a dict."""
-        vocab = {
-            self.convert_ids_to_tokens(i): i
-            for i in range(self.vocab_size)
-        }
+        vocab = {self.convert_ids_to_tokens(i): i for i in range(self.vocab_size)}
         vocab.update(self.added_tokens_encoder)
         return vocab
 
@@ -101,19 +113,19 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
 
     def _maybe_add_prefix_space(self, tokens, decoded):
         if tokens and tokens[0] not in self.no_prefix_space_tokens:
-            return ' ' + decoded
+            return " " + decoded
         else:
             return decoded
 
     def convert_tokens_to_string(self, tokens):
         """Converts a sequence of tokens (string) in a single string."""
         current_sub_tokens = []
-        out_string = ''
+        out_string = ""
         prev_is_special = False
         for token in tokens:
             if token in self.all_special_tokens:
                 if not prev_is_special:
-                    out_string += ' '
+                    out_string += " "
                 out_string += self.sp_model.decode(current_sub_tokens) + token
                 prev_is_special = True
                 current_sub_tokens = []
@@ -122,13 +134,10 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
                 prev_is_special = False
         out_string += self.sp_model.decode(current_sub_tokens)
         out_string = self.clean_up_tokenization(out_string)
-        out_string = self._maybe_add_prefix_space(
-            tokens=tokens, decoded=out_string)
+        out_string = self._maybe_add_prefix_space(tokens=tokens, decoded=out_string)
         return out_string[1:]
 
-    def save_vocabulary(self,
-                        save_directory, 
-                        filename_prefix: Optional[str] = None) -> Tuple[str]:
+    def save_vocabulary(self, save_directory, filename_prefix: Optional[str] = None) -> Tuple[str]:
         """Save the vocabulary and special tokens file to a directory.
 
         Args:
@@ -139,23 +148,21 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
             `Tuple(str)`: Paths to the files saved.
         """
         if not os.path.isdir(save_directory):
-            logger.error(
-                f'Vocabulary path ({save_directory}) should be a directory')
+            logger.error(f"Vocabulary path ({save_directory}) should be a directory")
             return
         out_vocab_file = os.path.join(
             save_directory,
-            (filename_prefix + '-' if filename_prefix else '') +
-            resource_files_names['vocab_file'])
+            (filename_prefix + "-" if filename_prefix else "") + self.resource_files_names["vocab_file"],
+        )
 
-        if os.path.abspath(self.vocab_file) != os.path.abspath(
-            out_vocab_file) and os.path.isfile(self.vocab_file):
+        if os.path.abspath(self.vocab_file) != os.path.abspath(out_vocab_file) and os.path.isfile(self.vocab_file):
             copyfile(self.vocab_file, out_vocab_file)
         elif not os.path.isfile(self.vocab_file):
-            with open(out_vocab_file, 'wb') as fi:
+            with open(out_vocab_file, "wb") as fi:
                 content_spiece_model = self.sp_model.serialized_model_proto()
                 fi.write(content_spiece_model)
 
-        return (out_vocab_file, )
+        return (out_vocab_file,)
 
     def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
         if self.add_bos_token:
@@ -174,10 +181,8 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
         return output
 
     def get_special_tokens_mask(
-        self,
-        token_ids_0: List[int],
-        token_ids_1: Optional[List[int]] = None,
-        already_has_special_tokens: bool = False) -> List[int]:
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None, already_has_special_tokens: bool = False
+    ) -> List[int]:
         """Retrieve sequence ids from a token list that has no special tokens
         added. This method is called when adding special tokens using the
         tokenizer `prepare_for_model` method.
@@ -195,19 +200,16 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
         """
         if already_has_special_tokens:
             return super().get_special_tokens_mask(
-                token_ids_0=token_ids_0,
-                token_ids_1=token_ids_1, 
-                already_has_special_tokens=True)
+                token_ids_0=token_ids_0, token_ids_1=token_ids_1, already_has_special_tokens=True
+            )
 
         if token_ids_1 is None:
             return [1] + ([0] * len(token_ids_0)) + [1]
-        return [1] + ([0] * len(token_ids_0)) + [1, 1] + (
-            [0] * len(token_ids_1)) + [1]
+        return [1] + ([0] * len(token_ids_0)) + [1, 1] + ([0] * len(token_ids_1)) + [1]
 
     def create_token_type_ids_from_sequences(
-        self, 
-        token_ids_0: List[int],
-        token_ids_1: Optional[List[int]] = None) -> List[int]:
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
         """Create a mask from the two sequences passed to be used in a
         sequence-pair classification task. T5 does not make use of token type
         ids, therefore a list of zeros is returned.
@@ -244,9 +246,7 @@ class InternLMXComposer2Tokenizer(PretrainedTokenizer):
             `List[str]`: The list of tokens.
         """
         # Simple mapping string => AddedToken for special tokens with specific tokenization behaviors
-        all_special_tokens_extended = dict(
-            (str(t), AddedToken(t)) for t in self.all_special_tokens_extended
-        )
+        all_special_tokens_extended = dict((str(t), AddedToken(t)) for t in self.all_special_tokens_extended)
 
         text, kwargs = self.prepare_for_tokenization(text, **kwargs)
 
