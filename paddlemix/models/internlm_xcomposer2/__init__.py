3d5ec4d4910e92ef0c65cc7cefbd20be0d687329
coco <69197635+cocoshe@users.noreply.github.com>
2024-04-28 10:33:30 +0800

    Add internlm-xcomposer2, forward and sft supported (#511)
    对齐 internlm-xcomposer2-7b 的前向，以及sft的step loss和step lr
    
    ---------
    
    Co-authored-by: LokeZhou <aishenghuoaiqq@163.com>

--- paddlemix/models/internlm_xcomposer2/__init__.py
+++ paddlemix/models/internlm_xcomposer2/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-# Copyright 2023 The Salesforce Team Authors and The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from .blip_processing import *
-from .clip_processing import *
-from .eva02_processing import *
-from .groundingdino_processing import *
-from .imagebind_processing import *
-from .internlm_xcomposer2_processing import *
-from .llava_processing import *
-from .minigpt4_image_processing import *
-from .minigpt4_processing import *
-from .qwen_vl_processing import *
-from .sam_processing import *
-from .tokenizer import SimpleTokenizer, tokenize
-from .visualglm_image_processing import *
-from .visualglm_processing import *
+from .configuration import *
+from .modeling import *
+from .tokenizer import *
