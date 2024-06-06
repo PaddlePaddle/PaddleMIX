fc08eec9925b0a05ac223d381e1d854b01cebe79
drryanhuang <zihaohuang@aliyun.com>
2024-06-06 10:51:14 +0800

    add hubert test

--- paddlemix/models/vits-svc/prepare/preprocess_hubert.py
+++ paddlemix/models/vits-svc/prepare/preprocess_hubert.py
@@ -35,6 +35,26 @@ def pred_vec(model, wavPath, vecPath):
 
 
 if __name__ == "__main__":
+
+    # # torch npy 路径
+    # tc_npy = "~/Desktop/whisper-vits-svc/data_svc/hubert/421_all/000002.vec.npy"
+    # tc_npy = os.path.expanduser(tc_npy)
+
+    # # paddle npy 路径
+    # pd_npy = "~/Desktop/PaddleMIX/paddlemix/models/vits-svc/data_svc/hubert/421_all/000002.vec.npy"
+    # pd_npy = os.path.expanduser(pd_npy)
+
+    # tc_arr = np.load(tc_npy)
+    # pd_arr = np.load(pd_npy)
+
+    # print(
+    #     abs(tc_arr - pd_arr).max(),
+    #     abs(tc_arr - pd_arr).mean(),
+    #     tc_arr.std() - pd_arr.std(),
+    #     tc_arr.mean() - pd_arr.mean(),
+    # )
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-w", "--wav", help="wav", dest="wav", default="data_svc/waves-16k/")
     parser.add_argument("-v", "--vec", help="vec", dest="vec", default="data_svc/hubert")
