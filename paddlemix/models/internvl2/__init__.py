75fa203b79663ae42aea6f444ef94e34f8d2f102
yinfan98 <32722923+yinfan98@users.noreply.github.com>
2024-07-28 22:33:23 +0800

    fix init

--- paddlemix/models/internvl2/__init__.py
+++ paddlemix/models/internvl2/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .configuration import *
-from .modeling import *
-from .tokenizer import *
+from .modeling_internvl_chat import *
+from .tokenizer_internlm2 import *
