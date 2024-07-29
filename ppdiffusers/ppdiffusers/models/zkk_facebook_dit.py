f653a66bac9ff75c4f32177a7acb7a3baf8bb300
changwenbin <changwenbin@baidu.com>
2024-07-29 12:53:59 +0000

    update zkk_facebook_dit.py

--- ppdiffusers/ppdiffusers/models/zkk_facebook_dit.py
+++ ppdiffusers/ppdiffusers/models/zkk_facebook_dit.py
@@ -1,18 +1,34 @@
 from paddle import nn
 import paddle
 import paddle.nn.functional as F
+import math
 
 class ZKKFacebookDIT(nn.Layer):
     def __init__(self, num_layers: int, dim: int, num_attention_heads: int, attention_head_dim: int):
         super().__init__()
         self.num_layers = num_layers
         self.dtype = "float16"
+        self.dim = dim
+        self.num_attention_heads = num_attention_heads
         self.attention_head_dim = attention_head_dim
-
-        self.fcs0 = nn.LayerList([nn.Linear(256, 1152) for i in range(self.num_layers)])
-        self.fcs1 = nn.LayerList([nn.Linear(1152, 1152) for i in range(self.num_layers)])
-        self.fcs2 = nn.LayerList([nn.Linear(1152, 6912) for i in range(self.num_layers)])
-        self.embs = nn.LayerList([nn.Embedding(1001, 1152) for i in range(self.num_layers)])
+        self.timestep_embedder_in_channels = 256
+        self.timestep_embedder_time_embed_dim = 1152
+        self.timestep_embedder_time_embed_dim_out = self.timestep_embedder_time_embed_dim
+        self.CombinedTimestepLabelEmbeddings_num_embeddings = 1001
+        self.CombinedTimestepLabelEmbeddings_embedding_dim = 1152
+        
+        self.fcs0 = nn.LayerList([nn.Linear(self.timestep_embedder_in_channels, 
+                                            self.timestep_embedder_time_embed_dim) for i in range(self.num_layers)])
+        
+        self.fcs1 = nn.LayerList([nn.Linear(self.timestep_embedder_time_embed_dim,
+                                            self.timestep_embedder_time_embed_dim_out) for i in range(self.num_layers)])
+        
+        self.fcs2 = nn.LayerList([nn.Linear(self.timestep_embedder_time_embed_dim,
+                                            6 * self.timestep_embedder_time_embed_dim) for i in range(self.num_layers)])
+        
+        self.embs = nn.LayerList([nn.Embedding(self.CombinedTimestepLabelEmbeddings_embedding_dim, 
+                                               self.CombinedTimestepLabelEmbeddings_num_embeddings) for i in range(self.num_layers)])
+        
 
         self.qkv = nn.LayerList([nn.Linear(dim, dim * 3) for i in range(self.num_layers)])
         self.out_proj = nn.LayerList([nn.Linear(dim, dim) for i in range(self.num_layers)])
@@ -24,46 +40,79 @@ class ZKKFacebookDIT(nn.Layer):
                           exp_enable_use_cutlass=True,
                           delete_pass_lists=["add_norm_fuse_pass"],
                         )
-    def forward(self,hidden_states, timestep, class_labels):
-
-        tmp = paddle.arange(dtype='float32', end=128)
-        tmp = tmp * -9.21034049987793 * 0.007874015718698502
-        tmp = paddle.exp(tmp).reshape([1,128])
-
-        timestep = timestep.cast("float32")
-        timestep = timestep.reshape([2,1])
-
-        tmp = tmp * timestep
-
-        tmp = paddle.concat([paddle.cos(tmp), paddle.sin(tmp)], axis=-1)
-        common_tmp = tmp.cast(self.dtype)
-
-        for i in range(self.num_layers):
-            tmp = self.fcs0[i](common_tmp)
-            tmp = F.silu(tmp)
-            tmp = self.fcs1[i](tmp)
-            tmp = tmp + self.embs[i](class_labels)
-            tmp = F.silu(tmp)
-            tmp = self.fcs2[i](tmp)
-            shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = tmp.chunk(6, axis=1)
+    def forward(self, hidden_states, timesteps, class_labels):
+        
+        # below code are copied from PaddleMIX/ppdiffusers/ppdiffusers/models/embeddings.py
+        num_channels = 256
+        max_period = 10000
+        downscale_freq_shift = 1
+        half_dim = num_channels // 2
+        exponent = -math.log(max_period) * paddle.arange(start=0, end=half_dim, dtype="float32")
+        exponent = exponent / (half_dim - downscale_freq_shift)
+        emb = paddle.exp(exponent)
+        emb = timesteps[:, None].cast("float32") * emb[None, :]
+        emb = paddle.concat([paddle.cos(emb), paddle.sin(emb)], axis=-1)
+        common_emb = emb.cast(self.dtype)
+        
+        for i in range(self.num_layers):  #$$  for?
+            emb = self.fcs0[i](common_emb)
+            emb = F.silu(emb)
+            emb = self.fcs1[i](emb)
+            emb = emb + self.embs[i](class_labels)
+            emb = F.silu(emb)
+            emb = self.fcs2[i](emb)
+            shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = emb.chunk(6, axis=1)
             norm_hidden_states = paddle.incubate.tt.adaptive_layer_norm(hidden_states, scale_msa, shift_msa)
             q,k,v = self.qkv[i](norm_hidden_states).chunk(3, axis=-1)
-            q = q.reshape([2,256,16,72])
-            k = k.reshape([2,256,16,72])
-            v = v.reshape([2,256,16,72])
-            
-            norm_hidden_states = F.scaled_dot_product_attention_(q, k, v, scale=self.attention_head_dim**-0.5)
-            norm_hidden_states = norm_hidden_states.reshape([2,256,1152])
-            norm_hidden_states = self.out_proj[i](norm_hidden_states)
+            b,s,h = q.shape
+            q = q.reshape([b,s,self.num_attention_heads,self.attention_head_dim])
+            k = k.reshape([b,s,self.num_attention_heads,self.attention_head_dim])
+            v = v.reshape([b,s,self.num_attention_heads,self.attention_head_dim])
 
-            hidden_states = hidden_states + norm_hidden_states * gate_msa.reshape([2,1,1152])
+            norm_hidden_states = F.scaled_dot_product_attention_(q, k, v, scale=self.attention_head_dim**-0.5)
+            norm_hidden_states = norm_hidden_states.reshape([b,s,self.dim])
+            norm_hidden_states = self.out_proj[i](norm_hidden_states)
             
-            norm_hidden_states = paddle.incubate.tt.adaptive_layer_norm(hidden_states, scale_mlp, shift_mlp)
+            # hidden_states = hidden_states + norm_hidden_states * gate_msa.reshape([b,1,self.dim])            
+            # norm_hidden_states = paddle.incubate.tt.adaptive_layer_norm(hidden_states, scale_mlp, shift_mlp)
+            
+            hidden_states,norm_hidden_states = paddle.incubate.tt.fused_adaLN_scale_residual(hidden_states, norm_hidden_states, gate_msa, scale_mlp, shift_mlp)
 
             norm_hidden_states = self.ffn1[i](norm_hidden_states)
             norm_hidden_states = F.gelu(norm_hidden_states, approximate=True)
             norm_hidden_states = self.ffn2[i](norm_hidden_states)
 
-            hidden_states = hidden_states + norm_hidden_states * gate_mlp.reshape([2,1,1152])
-        
+            hidden_states = hidden_states + norm_hidden_states * gate_mlp.reshape([b,1,self.dim])
+            
         return hidden_states
+    
+    
+        # tmp = paddle.arange(dtype='float32', end=128)
+        # tmp = tmp * -9.21034049987793 * 0.007874015718698502
+        # tmp = paddle.exp(tmp).reshape([1,128])
+        # timestep = timestep.cast("float32")
+        # timestep = timestep.reshape([2,1])
+        # tmp = tmp * timestep
+        # tmp = paddle.concat([paddle.cos(tmp), paddle.sin(tmp)], axis=-1)
+        # common_tmp = tmp.cast(self.dtype)
+        # for i in range(self.num_layers):
+        #     tmp = self.fcs0[i](common_tmp)
+        #     tmp = F.silu(tmp)
+        #     tmp = self.fcs1[i](tmp)
+        #     tmp = tmp + self.embs[i](class_labels)   
+        #     tmp = F.silu(tmp)
+        #     tmp = self.fcs2[i](tmp)
+        #     shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = tmp.chunk(6, axis=1)
+        #     norm_hidden_states = paddle.incubate.tt.adaptive_layer_norm(hidden_states, scale_msa, shift_msa)
+        #     q,k,v = self.qkv[i](norm_hidden_states).chunk(3, axis=-1)
+        #     orm_hidden_states = F.scaled_dot_product_attention_(q, k, v, scale=self.attention_head_dim**-0.5)
+        #     norm_hidden_states = norm_hidden_states.reshape([2,256,1152])
+        #     orm_hidden_states = self.out_proj[i](norm_hidden_states)
+        #     hidden_states = hidden_states + norm_hidden_states * gate_msa.reshape([2,1,1152])
+        #     norm_hidden_states = paddle.incubate.tt.adaptive_layer_norm(hidden_states, scale_mlp, shift_mlp)
+        #     norm_hidden_states = self.ffn1[i](norm_hidden_states)
+        #     norm_hidden_states = F.gelu(norm_hidden_states, approximate=True)
+        #     norm_hidden_states = self.ffn2[i](norm_hidden_states)
+        #     hidden_states = hidden_states + norm_hidden_states * gate_mlp.reshape([2,1,1152])
+        # return hidden_states
+        
