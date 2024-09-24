4b86c510ece1a9eb3c83a41afa31bdc9a6fca1e3
周周周 <39978853+zhoutianzi666@users.noreply.github.com>
2024-09-24 10:29:36 +0800

    SD3 transformer部分的优化 (#713)
    Co-authored-by: changwenbin <changwenbin@baidu.com>

--- ppdiffusers/examples/inference/text_to_image_generation-stable_diffusion_3.py
+++ ppdiffusers/examples/inference/text_to_image_generation-stable_diffusion_3.py
@@ -11,13 +11,104 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
+os.environ["FLAGS_use_cuda_managed_memory"] = "true"
+import argparse
+import datetime
 
 import paddle
+
 from ppdiffusers import StableDiffusion3Pipeline
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=" Use PaddleMIX to accelerate the Stable Diffusion3 image generation model."
+    )
+    parser.add_argument(
+        "--benchmark",
+        type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
+        default=False,
+        help="if benchmark is set to True, measure inference performance",
+    )
+    parser.add_argument(
+        "--inference_optimize",
+        type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
+        default=False,
+        help="If inference_optimize is set to True, all optimizations except Triton are enabled.",
+    )
+    parser.add_argument("--height", type=int, default=512, help="Height of the generated image.")
+    parser.add_argument("--width", type=int, default=512, help="Width of the generated image.")
+    parser.add_argument("--num-inference-steps", type=int, default=50, help="Number of inference steps.")
+    parser.add_argument("--dtype", type=str, default="float32", help="Inference data types.")
+
+    return parser.parse_args()
+
+
+args = parse_args()
+
+if args.inference_optimize:
+    os.environ["INFERENCE_OPTIMIZE"] = "True"
+    os.environ["INFERENCE_OPTIMIZE_TRITON"] = "True"
+if args.dtype == "float32":
+    inference_dtype = paddle.float32
+elif args.dtype == "float16":
+    inference_dtype = paddle.float16
+
 pipe = StableDiffusion3Pipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers", paddle_dtype=paddle.float16
+    "stabilityai/stable-diffusion-3-medium-diffusers",
+    paddle_dtype=inference_dtype,
 )
+
+pipe.transformer = paddle.incubate.jit.inference(
+    pipe.transformer,
+    save_model_dir="./tmp/sd3",
+    enable_new_ir=True,
+    cache_static_model=True,
+    exp_enable_use_cutlass=True,
+)
+
 generator = paddle.Generator().manual_seed(42)
 prompt = "A cat holding a sign that says hello world"
-image = pipe(prompt, generator=generator).images[0]
+
+
+image = pipe(
+    prompt, num_inference_steps=args.num_inference_steps, width=args.width, height=args.height, generator=generator
+).images[0]
+
+if args.benchmark:
+    # warmup
+    for i in range(3):
+        image = pipe(
+            prompt,
+            num_inference_steps=args.num_inference_steps,
+            width=args.width,
+            height=args.height,
+            generator=generator,
+        ).images[0]
+
+    repeat_times = 10
+    sumtime = 0.0
+    for i in range(repeat_times):
+        paddle.device.synchronize()
+        starttime = datetime.datetime.now()
+        image = pipe(
+            prompt,
+            num_inference_steps=args.num_inference_steps,
+            width=args.width,
+            height=args.height,
+            generator=generator,
+        ).images[0]
+        paddle.device.synchronize()
+        endtime = datetime.datetime.now()
+        duringtime = endtime - starttime
+        duringtime = duringtime.seconds * 1000 + duringtime.microseconds / 1000.0
+        sumtime += duringtime
+        print("SD3 end to end time : ", duringtime, "ms")
+
+    print("SD3 ave end to end time : ", sumtime / repeat_times, "ms")
+    cuda_mem_after_used = paddle.device.cuda.max_memory_allocated() / (1024**3)
+    print(f"Max used CUDA memory : {cuda_mem_after_used:.3f} GiB")
+
 image.save("text_to_image_generation-stable_diffusion_3-result.png")
---
+++ ppdiffusers/ppdiffusers/models/simplified_sd3.py
@@ -0,0 +1,161 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import paddle
+import paddle.nn.functional as F
+from paddle import nn
+
+
+class SimplifiedSD3(nn.Layer):
+    def __init__(self, num_layers: int, dim: int, num_attention_heads: int, attention_head_dim: int):
+        super().__init__()
+        self.num_layers = num_layers
+        self.dim = dim
+
+        self.silu = nn.Silu()
+        self.linear1 = nn.LayerList([nn.Linear(self.dim, 6 * self.dim) for i in range(num_layers)])
+        self.linear_context = nn.LayerList(
+            [nn.Linear(self.dim, (6 if i < num_layers - 1 else 2) * self.dim) for i in range(num_layers)]
+        )
+
+        self.norm_last_context = nn.LayerNorm(self.dim, epsilon=1e-6, weight_attr=False, bias_attr=True)
+
+        self.qkv = nn.LayerList([nn.Linear(self.dim, self.dim * 3) for i in range(num_layers)])
+        self.eqkv = nn.LayerList([nn.Linear(self.dim, self.dim * 3) for i in range(num_layers)])
+        self.to_out_linear = nn.LayerList([nn.Linear(self.dim, self.dim) for i in range(num_layers)])
+        self.to_add_out_linear = nn.LayerList([nn.Linear(self.dim, self.dim) for i in range(num_layers - 1)])
+        self.ffn1 = nn.LayerList([nn.Linear(self.dim, self.dim * 4) for i in range(num_layers)])
+        self.ffn2 = nn.LayerList([nn.Linear(self.dim * 4, self.dim) for i in range(num_layers)])
+        self.ffn1_context = nn.LayerList([nn.Linear(self.dim, self.dim * 4) for i in range(num_layers - 1)])
+        self.ffn2_context = nn.LayerList([nn.Linear(self.dim * 4, self.dim) for i in range(num_layers - 1)])
+
+    def forward(self, hidden_states, encoder_hidden_states, temb):
+        print("--------------------this is simplified_sd3------------------------")
+        temb_silu = self.silu(temb)
+
+        last_ffn_output = None
+        last_hidden_states = None
+        last_gate_mlp = None
+
+        last_context_ffn_output = None
+        last_context_hidden_states = None
+        last_context_gate_mlp = None
+
+        seq1 = hidden_states.shape[1]
+        seq2 = encoder_hidden_states.shape[1]
+
+        for i in range(self.num_layers):
+            context_pre_only = i == self.num_layers - 1
+
+            emb = self.linear1[i](temb_silu)
+            shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = emb.chunk(6, axis=1)
+
+            import paddlemix
+
+            if last_ffn_output is None:
+                norm_hidden_states = paddlemix.triton_ops.adaptive_layer_norm(
+                    hidden_states, scale_msa, shift_msa, epsilon=1e-06
+                )
+            else:
+                hidden_states, norm_hidden_states = paddlemix.triton_ops.fused_adaLN_scale_residual(
+                    last_hidden_states, last_ffn_output, last_gate_mlp, scale_msa, shift_msa, epsilon=1e-06
+                )
+
+            emb = self.linear_context[i](temb_silu)
+            if not context_pre_only:
+                shift_msa, scale_msa, c_gate_msa, c_shift_mlp, c_scale_mlp, c_gate_mlp = emb.chunk(6, axis=1)
+                if last_context_ffn_output is None:
+                    norm_encoder_hidden_states = paddlemix.triton_ops.adaptive_layer_norm(
+                        encoder_hidden_states, scale_msa, shift_msa, epsilon=1e-06
+                    )
+                else:
+                    (
+                        encoder_hidden_states,
+                        norm_encoder_hidden_states,
+                    ) = paddlemix.triton_ops.fused_adaLN_scale_residual(
+                        last_context_hidden_states,
+                        last_context_ffn_output,
+                        last_context_gate_mlp,
+                        scale_msa,
+                        shift_msa,
+                        epsilon=1e-06,
+                    )
+            else:
+                # the last layer.
+                scale, shift = paddle.chunk(emb, 2, axis=1)
+                (encoder_hidden_states, norm_encoder_hidden_states,) = paddlemix.triton_ops.fused_adaLN_scale_residual(
+                    last_context_hidden_states,
+                    last_context_ffn_output,
+                    last_context_gate_mlp,
+                    scale,
+                    shift,
+                    epsilon=1e-06,
+                )
+
+            qkv = self.qkv[i](norm_hidden_states)
+            eqkv = self.eqkv[i](norm_encoder_hidden_states)
+            q, k, v = paddlemix.triton_ops.split_concat(qkv, eqkv)
+            q = q.reshape([2, -1, 24, 64])
+            k = k.reshape([2, -1, 24, 64])
+            v = v.reshape([2, -1, 24, 64])
+
+            norm_hidden_states1 = F.scaled_dot_product_attention_(q, k, v, dropout_p=0.0, is_causal=False)
+            norm_hidden_states1 = norm_hidden_states1.reshape([2, -1, self.dim])
+            attn_output, context_attn_output = paddle.split(norm_hidden_states1, num_or_sections=[seq1, seq2], axis=1)
+
+            # attn_output, context_attn_output = paddlemix.triton_ops.triton_split(
+            #     norm_hidden_states1, num_or_sections=[1024, 154], axis=1
+            # )
+
+            attn_output = paddle.nn.functional.linear(
+                attn_output, self.to_out_linear[i].weight, self.to_out_linear[i].bias
+            )
+
+            if not context_pre_only:
+                context_attn_output = self.to_add_out_linear[i](context_attn_output)
+
+            hidden_states, norm_hidden_states = paddlemix.triton_ops.fused_adaLN_scale_residual(
+                hidden_states, attn_output, gate_msa, scale_mlp, shift_mlp, epsilon=1e-06
+            )
+
+            # ffn1
+            ffn_output = self.ffn1[i](norm_hidden_states)
+            ffn_output = F.gelu(ffn_output, approximate=True)
+            ffn_output = self.ffn2[i](ffn_output)
+
+            if context_pre_only:
+                ffn_output = gate_mlp.unsqueeze(1) * ffn_output
+                hidden_states = hidden_states + ffn_output
+            else:
+                last_ffn_output = ffn_output
+                last_hidden_states = hidden_states
+                last_gate_mlp = gate_mlp
+
+            # ffn2
+            if not context_pre_only:
+                (encoder_hidden_states, norm_encoder_hidden_states,) = paddlemix.triton_ops.fused_adaLN_scale_residual(
+                    encoder_hidden_states, context_attn_output, c_gate_msa, c_scale_mlp, c_shift_mlp, epsilon=1e-06
+                )
+
+                context_ffn_output = self.ffn1_context[i](norm_encoder_hidden_states)
+                context_ffn_output = F.gelu(context_ffn_output, approximate=True)
+                context_ffn_output = self.ffn2_context[i](context_ffn_output)
+
+                last_context_ffn_output = context_ffn_output
+                last_context_hidden_states = encoder_hidden_states
+                last_context_gate_mlp = c_gate_mlp
+            else:
+                encoder_hidden_states = None
+
+        return encoder_hidden_states, hidden_states
