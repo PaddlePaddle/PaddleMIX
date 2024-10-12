9d4da3b6d9c3253b99f46e224f44af5ed01d9a21
Xiaobin Lu <48754201+Xiaobin-Lu@users.noreply.github.com>
2024-10-12 10:55:42 +0800

    修复paddlenlp develop版本适配错误_10-11 (#735)

--- ppdiffusers/ppdiffusers/models/transformer_2d.py
+++ ppdiffusers/ppdiffusers/models/transformer_2d.py
@@ -355,7 +355,7 @@ class Transformer2DModel(ModelMixin, ConfigMixin):
             else:
                 batch, height, width, _ = hidden_states.shape
             residual = hidden_states
-
+            shape = paddle.shape(hidden_states)
             hidden_states = self.norm(hidden_states)
             if not self.use_linear_projection:
                 hidden_states = (
@@ -441,7 +441,10 @@ class Transformer2DModel(ModelMixin, ConfigMixin):
         # 3. Output
         if self.is_input_continuous:
             if not self.use_linear_projection:
-                hidden_states = hidden_states.reshape([batch, height, width, self.inner_dim])
+                if self.data_format == "NCHW":
+                    hidden_states = hidden_states.reshape([shape[0], shape[2], shape[3], self.inner_dim])
+                else:
+                    hidden_states = hidden_states.reshape([shape[0], shape[1], shape[2], self.inner_dim])
                 if self.data_format == "NCHW":
                     hidden_states = hidden_states.transpose([0, 3, 1, 2])
                 hidden_states = (
@@ -455,7 +458,10 @@ class Transformer2DModel(ModelMixin, ConfigMixin):
                     if not USE_PEFT_BACKEND
                     else self.proj_out(hidden_states)
                 )
-                hidden_states = hidden_states.reshape([batch, height, width, self.inner_dim])
+                if self.data_format == "NCHW":
+                    hidden_states = hidden_states.reshape([shape[0], shape[2], shape[3], self.inner_dim])
+                else:
+                    hidden_states = hidden_states.reshape([shape[0], shape[1], shape[2], self.inner_dim])
                 if self.data_format == "NCHW":
                     hidden_states = hidden_states.transpose([0, 3, 1, 2])
 
