f7e9df8e2114e5494410e69fe6e0c5577e338307
LokeZhou <aishenghuoaiqq@163.com>
2024-12-19 12:27:49 +0000

    open-magvit2 fix some error information

--- ppdiffusers/examples/visual_tokenizer/open-magvit2/README.md
+++ ppdiffusers/examples/visual_tokenizer/open-magvit2/README.md
@@ -6,13 +6,13 @@ This project provides the implementation of PaddlePaddle, support training and i
 #### Installation
 #### GPU
 - **Env**: We have tested on `Python 3.10` and `CUDA 11.8` (other versions may also be fine).
-- **PaddlePaddle** `Paddle3.0b`
+- **PaddlePaddle** `Paddle3.0beta1`
 - **Dependencies**: `pip install -r requirements.txt`
 
 
 #### Datasets
 
-We use Imagenet2012 as our dataset.
+We use Imagenet2012 as our dataset. download the dataset from [imagenet](http://www.image-net.org/), and put it into `data` directory:
 ```
 data
 └── train/
@@ -32,7 +32,7 @@ visual_tokenizer
 └── data
     └── train/
     └── val/
-└── open-magvit2 
+└── open-magvit2
     └── train_tokenizer.py
     ├── ...
 
@@ -65,7 +65,7 @@ Some important parameter configuration instructions, please refer to  `configs/g
 ```
 
 trainer:
-  precision: bfloat16  # float32 or bfloat16 
+  precision: bfloat16  # float32 or bfloat16
   max_epochs: 1        # max epochs
   max_steps: 10        # max steps
   num_sanity_val_steps: 5      # sanity check
@@ -82,7 +82,7 @@ ckpt_path: null  # to resume
 
 ####  Infer Scripts
 
-* $256\times 256$ reconstruct 
+* $256\times 256$ reconstruct
 ```
 wget https://bj.bcebos.com/v1/paddlenlp/models/community/paddlemix/imagenet_256_L.pdparams
 sh scripts/inference/reconstruct.sh
@@ -106,4 +106,3 @@ If you found the codebase and our work helpful, please cite it and give us a sta
   url={https://openreview.net/forum?id=gzqrANCF4g}
 }
 ```
-
--- ppdiffusers/examples/visual_tokenizer/open-magvit2/main.py
+++ ppdiffusers/examples/visual_tokenizer/open-magvit2/main.py
@@ -1,11 +1,26 @@
-import os
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import importlib
+
 import paddle
-import argparse, sys, datetime, glob, importlib
 
 custom_collate = paddle.io.dataloader.collate.default_collate_fn
 
+
 def get_obj_from_str(string, reload=False):
-    module, cls = string.rsplit('.', 1)
+    module, cls = string.rsplit(".", 1)
     if reload:
         module_imp = importlib.import_module(module)
         importlib.reload(module_imp)
@@ -13,13 +28,13 @@ def get_obj_from_str(string, reload=False):
 
 
 def instantiate_from_config(config):
-    if not 'target' in config:
-        raise KeyError('Expected key `target` to instantiate.')
-    return get_obj_from_str(config['target'])(**config.get('params', dict()))
+    if "target" not in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
 
 
 class WrappedDataset(paddle.io.Dataset):
-    """Wraps an arbitrary object with __len__ and __getitem__ into a pytorch dataset"""
+    """Wraps an arbitrary object with __len__ and __getitem__ into a paddle dataset"""
 
     def __init__(self, dataset):
         self.data = dataset
@@ -31,23 +46,20 @@ class WrappedDataset(paddle.io.Dataset):
         return self.data[idx]
 
 
-class DataModuleFromConfig():
-
-    def __init__(self, batch_size, train=None, validation=None, test=None,
-        wrap=False, num_workers=None):
+class DataModuleFromConfig:
+    def __init__(self, batch_size, train=None, validation=None, test=None, wrap=False, num_workers=None):
         super().__init__()
         self.batch_size = batch_size
         self.dataset_configs = dict()
-        self.num_workers = (num_workers if num_workers is not None else 
-            batch_size * 2)
+        self.num_workers = num_workers if num_workers is not None else batch_size * 2
         if train is not None:
-            self.dataset_configs['train'] = train
+            self.dataset_configs["train"] = train
             self.train_dataloader = self._train_dataloader
         if validation is not None:
-            self.dataset_configs['validation'] = validation
+            self.dataset_configs["validation"] = validation
             self.val_dataloader = self._val_dataloader
         if test is not None:
-            self.dataset_configs['test'] = test
+            self.dataset_configs["test"] = test
             self.test_dataloader = self._test_dataloader
         self.wrap = wrap
 
@@ -56,33 +68,34 @@ class DataModuleFromConfig():
             instantiate_from_config(data_cfg)
 
     def setup(self, stage=None):
-        self.datasets = dict((k, instantiate_from_config(self.
-            dataset_configs[k])) for k in self.dataset_configs)
+        self.datasets = dict((k, instantiate_from_config(self.dataset_configs[k])) for k in self.dataset_configs)
         if self.wrap:
             for k in self.datasets:
                 self.datasets[k] = WrappedDataset(self.datasets[k])
 
     def _train_dataloader(self):
-        return paddle.io.DataLoader(dataset=self.datasets['train'],
-            batch_size=self.batch_size, num_workers=self.num_workers,
-            shuffle=True, collate_fn=custom_collate)
+        return paddle.io.DataLoader(
+            dataset=self.datasets["train"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            collate_fn=custom_collate,
+        )
 
     def _val_dataloader(self):
-        return paddle.io.DataLoader(dataset=self.datasets['validation'],
-            batch_size=self.batch_size, num_workers=self.num_workers,
-            collate_fn=custom_collate, shuffle=False)
+        return paddle.io.DataLoader(
+            dataset=self.datasets["validation"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=custom_collate,
+            shuffle=False,
+        )
 
     def _test_dataloader(self):
-        return paddle.io.DataLoader(dataset=self.datasets['test'],
-            batch_size=self.batch_size, num_workers=self.num_workers,
-            collate_fn=custom_collate, shuffle=False)
-
-
-def main():
-    cli = lightning.pytorch.cli.LightningCLI(save_config_kwargs={
-        'overwrite': True})
-
-
-if __name__ == '__main__':
-    main()
-
+        return paddle.io.DataLoader(
+            dataset=self.datasets["test"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=custom_collate,
+            shuffle=False,
+        )
