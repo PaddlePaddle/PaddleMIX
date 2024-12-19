9f34c3d5989a3d7fc4aa9bbfb7b50fbb2c1c3145
swagger-coder <1581148245@qq.com>
2024-12-19 15:55:25 +0800

    fix llava_critic, llava_onevision precision bug

--- paddlemix/examples/llava_critic/run_predict.py
+++ paddlemix/examples/llava_critic/run_predict.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import copy
 
 import paddle
@@ -26,6 +27,7 @@ from paddlemix.models.llava.multimodal_encoder.siglip_encoder import (
     SigLipImageProcessor,
 )
 from paddlemix.models.llava.train_utils import tokenizer_image_token
+from paddlemix.utils.log import logger
 
 pretrained = "lmms-lab/llava-critic-7b"
 # pretrained = "lmms-lab/llava-onevision-qwen2-7b-chat"
@@ -65,3 +67,66 @@ cont = model.generate(
 )
 text_outputs = tokenizer.batch_decode(cont[0], skip_special_tokens=True)
 print("output:\n", text_outputs[0])
+
+
+def main(args):
+    compute_dtype = "float16" if args.fp16 else "bfloat16"
+    if "npu" in paddle.get_device():
+        is_bfloat16_supported = True
+    else:
+        is_bfloat16_supported = paddle.amp.is_bfloat16_supported()
+    if compute_dtype == "bfloat16" and not is_bfloat16_supported:
+        logger.warning("bfloat16 is not supported on your device,change to float32")
+        compute_dtype = "float32"
+
+    logger.info(f"compute_dtype: {compute_dtype}")
+
+    model = LlavaQwenForCausalLM.from_pretrained(args.model_path, dtype=compute_dtype).eval()
+    tokenizer = Qwen2Tokenizer.from_pretrained(pretrained)
+    image_processor = SigLipImageProcessor()
+
+    image = Image.open(args.image_file)
+    image_tensor = process_images([image], image_processor, model.config)
+    image_tensor = [_image.cast(paddle.bfloat16) for _image in image_tensor]
+
+    conv_template = args.conv_mode  # Make sure you use correct chat template for different models
+
+    # pairwise ranking
+    critic_prompt = args.critic_prompt
+
+    # pointwise scoring
+    # critic_prompt = "Given an image and a corresponding question, please serve as an unbiased and fair judge to evaluate the quality of answer answers provided by a Large Multimodal Model (LMM). Score the response out of 100 and explain your reasoning with specific details. Your task is provided as follows:\nQuestion: [What this image presents?]\nThe LMM response: [This is a handwritten number seven.]\nASSISTANT:\n "
+
+    question = DEFAULT_IMAGE_TOKEN + "\n" + critic_prompt
+    conv = copy.deepcopy(conv_templates[conv_template])
+    conv.append_message(conv.roles[0], question)
+    conv.append_message(conv.roles[1], None)
+    prompt_question = conv.get_prompt()
+
+    input_ids = tokenizer_image_token(prompt_question, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pd").unsqueeze(0)
+    image_sizes = [image.size]
+
+    cont = model.generate(
+        input_ids,
+        images=image_tensor,
+        image_sizes=image_sizes,
+        do_sample=False,
+        temperature=args.temperature,
+        max_new_tokens=args.max_new_tokens,
+    )
+    text_outputs = tokenizer.batch_decode(cont[0], skip_special_tokens=True)
+    print("output:\n", text_outputs[0])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="lmms-lab/llava-critic-7b")
+    parser.add_argument("--critic_prompt", type=str, default="Given an image and a corresponding question, please serve as an unbiased and fair judge to evaluate the quality of the answers provided by a Large Multimodal Model (LMM). Determine which answer is better and explain your reasoning with specific details. Your task is provided as follows:\nQuestion: [What this image presents?]\nThe first response: [The image is a black and white sketch of a line that appears to be in the shape of a cross. The line is a simple and straightforward representation of the cross shape, with two straight lines intersecting at a point.]\nThe second response: [This is a handwritten number seven.]\nASSISTANT:\n")
+    parser.add_argument("--image_file", type=str, default="paddlemix/demo_images/critic_img_seven.png")
+    parser.add_argument("--conv_mode", type=str, default="qwen_1_5")
+    parser.add_argument("--temperature", type=float, default=0.2)
+    parser.add_argument("--max_new_tokens", type=int, default=1024)
+    parser.add_argument("--fp16", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+    main(args)
