9550644206a47b661b9bbafd64858bdd981ef1ec
lyuwenyu <wenyu.lyu@gmail.com>
2024-12-19 15:31:16 +0800

    pp infinity doc data

---
+++ paddlemix/datacopilot/example/pp_infinitydocdata/run.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from pathlib import Path
+from functools import partial
+
+from paddlemix.datacopilot.core import MMDataset
+from paddlemix.datacopilot.nn.llms import PaddleGPT4o
+from paddlemix.datacopilot.nn.layout_parsing import PaddleXLayoutParser
+from paddlemix.datacopilot.ops.generate.pp_infinity_doc import PPInfinityDocData
+
+
+def generate_for_single_image(item: dict, generator: PPInfinityDocData):    
+    conversations = generator.generate_doc(item['layout'])
+    return item.update({'conversations': conversations})
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type=str, required=True, )
+    parser.add_argument('--output_json_path', type=str, required=True, )
+    parser.add_argument('--gpu_id', type=int, default=0, )
+    parser.add_argument('--num_gpus', type=int, default=1, )
+    parser.add_argument('--processes_per_gpu', type=int, default=1, )
+
+    args = parser.parse_args()
+
+    generator = PPInfinityDocData(llm=PaddleGPT4o())
+    layout_parser = PaddleXLayoutParser(gpu_id=args.gpu_id)
+    
+    paths = list(sorted(Path(args.root).glob('*.jpg')))
+
+    layouts = layout_parser.process_images(paths, num_gpus=args.num_gpus, processes_per_gpu=args.processes_per_gpu)
+    items = [dict(image=path, layout=layout) for path, layout in zip(paths, layouts) if layout]
+
+    (
+        MMDataset(items)
+        .map(partial(generate_for_single_image, generator=generator))
+        .nonempty()
+        .export_json(args.output_json_path)
+    )
+
+
+
---
+++ paddlemix/datacopilot/nn/llms.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import time
+import random
+import requests
+
+
+class PaddleGPT4o:
+    def __init__(self, ):
+        pass
+
+    def get_output(self, prompts, ):
+        while True:
+            rst = self.get_result(
+                text=prompts,
+                max_tokens=2048,
+                temperature=0.9,
+                topp=0,
+                penalty_score=1,
+            )
+            if len(rst) > 1:
+                break
+            time.sleep(1)
+        return rst
+
+    def gen_session_id(self, prefix="1"):
+        # 生成新Session ID
+        return str(prefix) + "session_id_time_%f_rand_%f" % (time.time(), random.random())
+
+    def request_gpt4(self, json_data):    
+        response = requests.request("POST", "http://10.88.94.144:8900/generate", json=json_data).json()
+        return response
+    
+    def get_result(self, text, max_tokens, temperature, topp, penalty_score, history=None):
+        request_data = {
+            "context": [
+                {
+                    "role": "system",
+                    "utterance": [{
+                        "type": "text",
+                        "text": text[0],
+                        },
+                    ],
+                },
+                {
+                    "role": "user", 
+                    "utterance": [{
+                        "type": "text",
+                        "text": text[1],
+                        },
+                    ],
+                }
+            ],
+            "top_p": topp,
+            "temperature": temperature,
+            "penalty_score": penalty_score,
+            "frequency_score": 0,
+            "presence_score": 0,
+            "min_dec_len": 2,
+            "max_dec_len": max_tokens,
+        }
+    
+        SUCCESS = False
+        session_id = self.gen_session_id()
+
+        try:
+            rst = self.request_gpt4(request_data)
+            if 'result' in rst:
+                if 'response' in rst['result']:
+                    if 'utterance' in rst['result']['response']:
+                        SUCCESS = True
+            result = rst['result']['response']['utterance']
+        
+        except Exception as e:
+            print("error:", e, "session_id:", session_id)
+        if SUCCESS:
+            return result
+        else:
+            return ""
