7b89eb637ec7e94a7575a175e6cfda8ed10db3a2
LielinJiang <jianglielin@baidu.com>
2025-03-07 12:15:02 +0000

    fix build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -21,16 +21,16 @@ echo "开始安装 PaddleMIX 及其依赖..."
 
 # 安装 PaddleMIX
 echo "安装 PaddleMIX..."
-pip install -e . --user
+pip install -e .
 
 # 安装 ppdiffusers
 echo "安装 ppdiffusers..."
 cd ppdiffusers
-pip install -e . --user
+pip install -e .
 cd ..
 #注：ppdiffusers部分模型需要依赖 CUDA 11.2 及以上版本，如果本地机器不符合要求，建议前往 [AI Studio](https://aistudio.baidu.com/index) 进行模型训练、推理任务。
 #如果希望使用**bf16**训练推理，请使用支持**bf16**的GPU，如A100。
 
 # 安装依赖包
 echo "安装依赖包..."
-pip install -r requirements.txt --user
+pip install -r requirements.txt
