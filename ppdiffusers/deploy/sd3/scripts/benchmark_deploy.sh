76efef188e98464d3febdeb05c9f5027bd64d647
chang-wenbin <changwenbin@baidu.com>
2025-03-11 14:08:21 +0800

    del pir=0, use PIR

--- ppdiffusers/deploy/sd3/scripts/benchmark_deploy.sh
+++ ppdiffusers/deploy/sd3/scripts/benchmark_deploy.sh
@@ -9,10 +9,8 @@ cd PaddleMIX
 pip install -e .
 cd PaddleMIX/ppdiffusers/deploy/sd3
 
-export FLAGS_enable_pir_api=0
 # text_to_image_generation-stable_diffusion_3.py中设置exp_enable_use_cutlass=False
 python  text_to_image_generation-stable_diffusion_3.py  --dtype float16 --height 1024 --width 1024 --num-inference-steps 50 --inference_optimize 1  --benchmark yes
-
 
 # 执行多卡推理指令
 python -m paddle.distributed.launch --gpus "0,1,2,3" text_to_image_generation-stable_diffusion_3.py \
