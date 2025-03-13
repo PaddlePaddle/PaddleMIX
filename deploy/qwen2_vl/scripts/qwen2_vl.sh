73059b37f18463745772cddc86237b789a0dd884
chang-wenbin <changwenbin@baidu.com>
2025-03-13 17:55:04 +0800

    support qwen parallel run

--- deploy/qwen2_vl/scripts/qwen2_vl.sh
+++ deploy/qwen2_vl/scripts/qwen2_vl.sh
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export CUDA_VISIBLE_DEVICES=0,1
 #fp16  高性能推理
-python deploy/qwen2_vl/single_image_infer.py \
-    --model_name_or_path Qwen/Qwen2-VL-2B-Instruct \
+python deploy/qwen2_vl/single_image_infer.py\
+    --model_name_or_path Qwen/Qwen2-VL-7B-Instruct \
     --question "Describe this image." \
     --image_file paddlemix/demo_images/examples_image1.jpg \
     --min_length 128 \
@@ -23,9 +24,12 @@ python deploy/qwen2_vl/single_image_infer.py \
     --top_p 0.001 \
     --temperature 0.1 \
     --repetition_penalty 1.05 \
+    --block_attn True \
+    --inference_model True \
+    --mode dynamic \
     --dtype bfloat16 \
     --benchmark True 
-    
+
 
 
 # # weight only int8 量化推理
@@ -39,6 +43,26 @@ python deploy/qwen2_vl/single_image_infer.py \
 #     --top_p 0.001 \
 #     --temperature 0.1 \
 #     --repetition_penalty 1.05 \
+#     --block_attn True \
+#     --inference_model True \
+#     --mode dynamic \
 #     --dtype bfloat16 \
 #     --quant_type "weight_only_int8" \
+#     --benchmark True
+
+# # 多卡推理功能
+# python -m paddle.distributed.launch --gpus "0,1" deploy/qwen2_vl/single_image_infer.py \
+#     --model_name_or_path Qwen/Qwen2-VL-7B-Instruct \
+#     --question "Describe this image." \
+#     --image_file paddlemix/demo_images/examples_image1.jpg \
+#     --min_length 128 \
+#     --max_length 128 \
+#     --top_k 1 \
+#     --top_p 0.001 \
+#     --temperature 0.1 \
+#     --repetition_penalty 1.05 \
+#     --block_attn True \
+#     --inference_model True \
+#     --mode dynamic \
+#     --dtype bfloat16 \
 #     --benchmark True 
