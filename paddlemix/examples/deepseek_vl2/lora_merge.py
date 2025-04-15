a56305712f4f56e49086d7cb51e7cd9d147a0b80
cheng221 <jcl_hqu@outlook.com>
2025-04-15 11:25:29 +0800

    Fix code format

--- paddlemix/examples/deepseek_vl2/lora_merge.py
+++ paddlemix/examples/deepseek_vl2/lora_merge.py
@@ -1,9 +1,23 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 
 import paddle
 from paddlenlp.peft import LoRAConfig, LoRAModel
-
 from paddlenlp.transformers import DeepseekTokenizerFast
+
 from paddlemix.models.deepseek_vl2 import DeepseekVLV2ForCausalLM
 
 
@@ -38,7 +52,7 @@ def merge():
     for key in list(model_state_dict):
         if "lora" in key:
             del model_state_dict[key]
-    model.model.save_pretrained(args.merge_model_path, state_dict=model_state_dict,safe_serialization=True)
+    model.model.save_pretrained(args.merge_model_path, state_dict=model_state_dict, safe_serialization=True)
 
     # save tokenizer config
     tokenizer = DeepseekTokenizerFast.from_pretrained(args.model_name_or_path)
