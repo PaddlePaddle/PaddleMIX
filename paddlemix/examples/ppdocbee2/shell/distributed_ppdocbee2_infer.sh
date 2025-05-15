0a7c7e36670c175c6b8f04263f2b307f2baedc02
pkhk-1 <kui.311@qq.com>
2025-05-15 08:21:54 +0000

    rename ppdocbee2

--- paddlemix/examples/ppdocbee2/shell/distributed_ppdocbee2_infer.sh
+++ paddlemix/examples/ppdocbee2/shell/distributed_ppdocbee2_infer.sh
@@ -15,5 +15,5 @@
 # 使用 Paddle 分布式启动推理
 python -m paddle.distributed.launch --gpus="0,1" \
     paddlemix/examples/ppdocbee2/distributed_ppdocbee2_infer.py \
-    --model_path PaddleMIX/PPDocBeeV2-3B \
+    --model_path PaddleMIX/PPDocBee2-3B \
     --mp_degree 2
